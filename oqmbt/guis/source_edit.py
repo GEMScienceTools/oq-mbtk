9586b73ae65ad4aa864dd26375e91e9a702f9fee
Marco Pagani <marco.pagani@globalquakemodel.org>
2017-12-14 12:09:35 +0100

    First version

---
+++ oqmbt/guis/source_edit.py
@@ -0,0 +1,134 @@
+"""
+Module :mod:`oqmbt.guis.project_select` defines the gui setting the
+fundamental parameters of a project used to run scripts.
+"""
+
+import os
+from ipywidgets import widgets
+from oqmbt.guis.project_select import (_get_source_types,
+                                       handle_change, handle_change_type,
+                                       handle_click)
+MARGIN = 5
+SKIP_SET = set(['polygon'])
+FIXED_SET = set(['source_type'])
+
+
+def source_editor_gui(project):
+    """
+    :parameter project:
+        An instance of a :class:`oqmbt.project.OQtProject`
+    """
+    global oqmbtp
+    global model
+    global w_name, w_dir, w_model, w_sources, project_dir, w_types
+
+    oqmbtp = project
+    models = oqmbtp.models.keys()
+    project_dir = oqmbtp.directory
+    project_pickle_filename = os.environ.get('OQMBT_PROJECT')
+
+    wdg_list = []
+    w_title = widgets.HTML(value="<h3>Edit source parameters<h3>")
+    # Project name
+    tmp_str = "Project:<br>"
+    tmp_str += "<small>%s</small><br>" % (project_dir)
+    # Filename
+    tmp_str += "Filename: <br>"
+    tmp_str += "<small>%s</small><br><br>" % (
+        os.path.split(project_pickle_filename)[1])
+    w_dir = widgets.HTML(value=tmp_str)
+    w_name = widgets.Text(description='Name',
+                          value=oqmbtp.name,
+                          width=600,
+                          margin=MARGIN)
+    wdg_list.append(w_title)
+    wdg_list.append(w_dir)
+    wdg_list.append(w_name)
+
+    if len(models):
+        model_id = models[0]
+        model = oqmbtp.models[model_id]
+        w_model = widgets.Dropdown(options=models,
+                                   description='Model',
+                                   value=model_id)
+        if len(model.sources.keys()):
+            types = sorted(_get_source_types(model))
+            types.insert(0, 'All')
+            # Create the list of Text widgets
+            param_list = get_source_param_list()
+            print param_list
+            w_text_list, par_dict = param_editor_create(param_list)
+            # Source types widget
+            w_types = widgets.Select(options=types,
+                                     description='Types',
+                                     margin=MARGIN,
+                                     height=50,
+                                     value=types[0])
+            keys_list = sorted(model.sources.keys())
+            w_sources = widgets.Select(options=keys_list,
+                                       description='Sources',
+                                       value=keys_list[0],
+                                       margin=MARGIN)
+            # Set parameters
+            param_set(param_list, par_dict)
+        else:
+            w_sources = widgets.SelectMultiple(options=[],
+                                               description='Sources:',
+                                               margin=MARGIN)
+            w_types = widgets.Select(options=[],
+                                     description='Types',
+                                     margin=MARGIN)
+        wdg_list.append(w_model)
+        wdg_list.append(w_types)
+        wdg_list.append(w_sources)
+        wdg_list = wdg_list + w_text_list
+
+    w_butt = widgets.Button(description='Done!', width=100, border_color='red')
+    wdg_list.append(w_butt)
+
+    w_types.on_trait_change(handle_change_type, 'value')
+    w_model.on_trait_change(handle_change)
+    w_butt.on_click(handle_click)
+
+    return widgets.VBox(children=wdg_list)
+
+
+def get_source_param_list():
+    """
+    Get the list of parameters used to descibe the sources in a given model.
+    In the future this should be probably added to the project description.
+    """
+    par_set = set()
+    for key in model.sources:
+        src = model.sources[key]
+        for par in src.__dict__:
+            if not SKIP_SET & set([par]):
+                par_set = par_set | set([par])
+    return list(par_set)
+
+
+def param_editor_create(par_list):
+    """
+    This creates a list of text widgets given a list of parameters
+    """
+    wdg_list = []
+    wdg_dict = {}
+    wdg_list = [widgets.HTML(value="<h4>Source parameters<h4>")]
+    for par in par_list:
+        wdg = widgets.Text(description=par,
+                           margin=MARGIN,
+                           width=500)
+        wdg_list.append(wdg)
+        wdg_dict[par] = wdg
+        del wdg
+    return wdg_list, wdg_dict
+
+
+def param_set(par_list, wdg_dict):
+    model_id = w_model.value
+    model = oqmbtp.models[model_id]
+    src_id = w_sources.value
+    src = model.sources[src_id]
+    for key in par_list:
+        wdg = wdg_dict[key]
+        wdg.value = getattr(src, key)
---
+++ oqmbt/tools/area.py
@@ -0,0 +1,190 @@
+
+import numpy
+from shapely import wkt
+
+from osgeo import ogr
+from copy import deepcopy
+
+from prettytable import PrettyTable
+
+from openquake.hmtk.seismicity.selector import CatalogueSelector
+from openquake.hmtk.sources.area_source import mtkAreaSource
+
+from oqmbt.tools.general import _get_point_list
+from oqmbt.oqt_project import OQtSource
+
+from openquake.hazardlib.geo.polygon import Polygon
+
+
+def load_geometry_from_shapefile(shapefile_filename):
+    """
+    :parameter str shapefile_filename:
+        Name of the shapefile containing the polygons
+    :parameter Boolean log:
+        Flag controlling information while processing
+    :returns:
+        A list of :class:`oqmbt.oqt_project.OQtSource` istances
+    """
+    idname = 'Id'
+    # Set the driver
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    dataSource = driver.Open(shapefile_filename, 0)
+    layer = dataSource.GetLayer()
+    # Reading sources geometry
+    sources = {}
+    id_set = set()
+    for feature in layer:
+        # Read the geometry
+        geom = feature.GetGeometryRef()
+        polygon = wkt.loads(geom.ExportToWkt())
+        x, y = polygon.exterior.coords.xy
+        points = _get_point_list(x, y)
+        # Set the ID
+        if isinstance(feature.GetField(idname), str):
+            id_str = feature.GetField(idname)
+        elif isinstance(feature.GetField(idname), int):
+            id_str = '%d' % (feature.GetField(idname))
+        else:
+            raise ValueError('Unsupported source ID type')
+        src = OQtSource(source_id=id_str,
+                        source_type='AreaSource',
+                        polygon=Polygon(points),
+                        name=id_str,
+                        )
+        # Append the new source
+        if not id_set and set(id_str):
+            sources[id_str] = src
+        else:
+            raise ValueError('Sources with non unique ID %s' % id_str)
+    return sources
+
+
+def src_oqt_to_hmtk(src):
+    return mtkAreaSource(
+            identifier=src.source_id,
+            name=src.name,
+            geometry=src.polygon)
+
+def create_catalogue(model, catalogue, area_source_ids_list=None,
+                                         polygon=None):
+    """
+    Note that this assumes that the catalogue has a rtree spatial index
+    associated.
+
+    :parameter model:
+    :parameter catalogue:
+    :parameter area_source_ids_list:
+    """
+
+    if area_source_ids_list is not None:
+        # Process the area source
+        src_id = area_source_ids_list[0]
+        src = model.sources[src_id]
+        # Check if the area source has a geometry
+        if 'polygon' in src.__dict__:
+            pass
+        elif src_id in model.nrml_sources:
+            src.polygon = model.nrml_sources[src_id].polygon
+            src.name = model.nrml_sources[src_id].name
+            src.source_id = model.nrml_sources[src_id].source_id
+        else:
+            print ('The source does not have a geometry assigned')
+            return None
+    elif polygon is not None:
+        assert isinstance(polygon, Polygon)
+        src_id = 'user_defined'
+        src = OQtSource('id', 'AreaSource')
+        src.name = 'dummy'
+        src.polygon = polygon
+    else:
+            msg = 'Either a polygon or a list of src id must be defined'
+            raise ValueError(msg)
+
+    # This sets the limits of the area covered by the polygon
+    limits = [numpy.min(src.polygon.lons),
+              numpy.min(src.polygon.lats),
+              numpy.max(src.polygon.lons),
+              numpy.max(src.polygon.lats)]
+    # Src hmtk
+    src_hmtk = src_oqt_to_hmtk(src)
+    # This creates a new catalogue with eqks within the bounding box of
+    # the analysed area source
+    selectorB = CatalogueSelector(catalogue, create_copy=True)
+    tmpcat = selectorB.within_bounding_box(limits)
+    selectorA = CatalogueSelector(tmpcat, create_copy=False)
+    # This filters out the eqks outside the area source
+    src_hmtk.select_catalogue(selectorA)
+    # Create the composite catalogue as a copy of the sub-catalogue for the first source
+    labels = ['%s' % src_id for i in range(0, len(src_hmtk.catalogue.data['magnitude']))]
+    src_hmtk.catalogue.data['comment'] = labels
+    fcatal = deepcopy(src_hmtk.catalogue)
+    print ('merging eqks for source:', src_id, '# eqks:', len(src_hmtk.catalogue.data['magnitude']))
+    # Complete the composite subcatalogue
+    """
+    for src_id in area_source_ids_list[1:]:
+        # Set the source index and fix the catalogue selector
+        src = model.sources[src_id]
+        src_hmtk = src_oqt_to_hmtk(src)
+        # Merge the source-subcatalogue to the composite one
+        # print 'merging eqks for source:', src_id, '# eqks:', len(src_hmtk.catalogue.data['magnitude'])
+        labels = ['%s' % src_id for i in range(0, len(src_hmtk.catalogue.data['magnitude']))]
+        src_hmtk.catalogue.data['comment'] = labels
+        fcatal.concatenate(src.catalogue)
+    """
+    print ('Total number of earthquakes selected {:d}'.format(
+                fcatal.get_number_events()))
+    return fcatal
+
+
+def create_gr_table(model):
+        # Set table
+        p = PrettyTable(["ID","a_gr", "b_gr"])
+        p.align["Source ID"] = 'l'
+        p.align["a_gr"] = 'r'
+        p.align["b_gr"] = 'r'
+        #
+        for key in sorted(model.sources):
+            src = model.sources[key]
+            if src.source_type == 'AreaSource':
+                alab = ''
+                blab = ''
+                if 'a_gr' in src.__dict__:
+                    alab = '%8.5f' % (src.a_gr)
+                if 'b_gr' in src.__dict__:
+                    blab = '%6.3f' % (src.b_gr)
+                p.add_row([key, alab, blab])
+        return p
+
+def create_mmax_table(model):
+        # Set table
+        p = PrettyTable(["ID","mmax obs", "mmax assigned", "mo strain"])
+        p.align["Source ID"] = 'l'
+        p.align["mmax obs"] = 'r'
+        p.align["mmax assigned"] = 'r'
+        p.align["mo strain"] = 'r'
+        #
+        for key in sorted(model.sources):
+            src = model.sources[key]
+            if src.source_type == 'AreaSource':
+                alab = ''
+                blab = ''
+                clab = ''
+                if src.__dict__.has_key('mmax_obs'):
+                    alab = '%6.2f' % (src.mmax_obs)
+                if src.__dict__.has_key('mmax_expected'):
+                    blab = '%6.2f' % (src.mmax_expected)
+                if src.__dict__.has_key('mo_strain'):
+                    clab = '%6.2e' % (src.mo_strain)
+                p.add_row([key, alab, blab, clab])
+        return p
+
+def plot_area_source_polygons(model, bmap):
+        """
+        :parameter bmap:
+                A :class:Basemap instance
+        """
+        for key in sorted(model.sources):
+            src = model.sources[key]
+            if src.source_type == 'AreaSource':
+                        x, y = bmap(src.polygon.lons, src.polygon.lats)
+                        bmap.plot(x, y, '-b')
