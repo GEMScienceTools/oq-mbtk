ab61e9bfe2e2020e837d628ee1d38d9876930faf
Marco Pagani <marco.pagani@globalquakemodel.org>
2018-01-19 11:58:58 +0100

    Updated some files + added a first version of setup.py still requiring test

--- oqmbt/tests/tools/smooth3d_test.py
+++ oqmbt/tests/tools/smooth3d_test.py
@@ -2,9 +2,10 @@ import numpy as np
 import unittest
 
 from pyproj import Proj
-from mbt.tools.smooth3d import Smoothing3D
+from oqmbt.tools.smooth3d import Smoothing3D
 from openquake.hazardlib.geo.mesh import Mesh
 from openquake.hmtk.seismicity.catalogue import Catalogue
+
 
 class Smooth3DTestCase(unittest.TestCase):
     """
@@ -31,10 +32,10 @@ class Smooth3DTestCase(unittest.TestCase):
                 for z in np.arange(0, 20+dlt, self.spcv):
                     pnts.append([x, y, z])
         pnts = np.array(pnts)
-        plo, pla = self.p(pnts[:,0]*1e3, pnts[:,1]*1e3, inverse=True)
-        pnts[:,0] = plo
-        pnts[:,1] = pla
-        self.mesh = Mesh(pnts[:,0], pnts[:,1], pnts[:,2])
+        plo, pla = self.p(pnts[:, 0]*1e3, pnts[:, 1]*1e3, inverse=True)
+        pnts[:, 0] = plo
+        pnts[:, 1] = pla
+        self.mesh = Mesh(pnts[:, 0], pnts[:, 1], pnts[:, 2])
         #
         # create a catalogue
         keys = ['longitude', 'latitude', 'depth', 'year', 'magnitude']
@@ -49,23 +50,20 @@ class Smooth3DTestCase(unittest.TestCase):
         """
         smooth = Smoothing3D(self.cat, self.mesh, self.spch, self.spcv)
         values = smooth.gaussian(20, [5, 5, 2])
-        print ('sum:', sum(values))
+        print('sum:', sum(values))
 
         if False:
+            vsc = 0.01
             import matplotlib.pyplot as plt
             from mpl_toolkits.mplot3d import Axes3D
             fig = plt.figure()
             iii = np.nonzero(values > 1e-15)[0]
             ax = fig.add_subplot(111, projection='3d')
-            #ax.plot(catalogue.data['longitude'], catalogue.data['latitude'],
-            #        catalogue.data['depth'], 's', alpha=0.1)
-            ax.plot(self.mesh.lons, self.mesh.lats, self.mesh.depths, '.k',
+            ax.plot(self.mesh.lons, self.mesh.lats, self.mesh.depths*vsc, '.k',
                     alpha=0.2)
             ax.scatter(self.mesh.lons[iii], self.mesh.lats[iii],
-                       self.mesh.depths[iii], c=np.log10(values[iii]),
+                       self.mesh.depths[iii]*vsc, c=np.log10(values[iii]),
                        alpha=0.5)
             ax.invert_zaxis()
             plt.show()
-
-
         assert 0 == 1
