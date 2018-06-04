f69441f5c9f6ef767bbfec2711b795ebf582323b
Marco Pagani <marco.pagani@globalquakemodel.org>
2018-06-04 22:19:33 +0200

    Adding tests

---
+++ openquake/mbt/tests/tools/tr/tr02_test.py
@@ -0,0 +1,45 @@
+import os
+import h5py
+import shutil
+import numpy
+import unittest
+
+from openquake.mbt.tools.tr.classify import classify
+
+BASE_PATH = os.path.dirname(__file__)
+
+
+class TrTestCase02(unittest.TestCase):
+    """
+    This class tests the tectonic regionalisation workflow
+    """
+
+    def setUp(self):
+        self.root_folder = os.path.join(BASE_PATH)
+        self.tmp = os.path.join(BASE_PATH, '../../tmp/')
+        if os.path.exists(self.tmp):
+            shutil.rmtree(self.tmp)
+        os.makedirs(self.tmp)
+
+    def tearDown(self):
+        #
+        # removing tmp folder
+        # shutil.rmtree(self.tmp)
+        pass
+
+    def testcase01(self):
+        """
+        Testing TR - Case 02 - Crustal
+        """
+        ini_fname = os.path.join(BASE_PATH, '../../data/tr02/cls_v2_1500_2017.ini')
+        treg_filename = os.path.join(BASE_PATH, '../../tmp/test02.hdf5')
+        #
+        # classify
+        classify(ini_fname, True, self.root_folder)
+        f = h5py.File(treg_filename, 'r')
+        #
+        # testing crustal active
+        expected = [1, 1, 0, 0, 0, 0]
+        numpy.testing.assert_array_equal(f['crustal'][:], expected)
+        #
+        f.close()
