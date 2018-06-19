a09471fc8360e9f291a0b1c3754e0e3d9b738319
Kendra Johnson <kjohnson@Kendras-MacBook-Air.local>
2018-06-19 17:49:10 +0200

    adding function to change classification

---
+++ openquake/mbt/tools/tr/change_class.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import sys
+import h5py
+import pickle
+import re
+
+from openquake.baselib import sap
+
+def change(cat_pickle_filename,treg,eqlist):
+    """
+    cat_pickle_filename - pickled catalogue
+    treg - hdf5 file with classifications; currently a new file '<>_up.hdf5' is created
+    rather than overwriting
+    eqlist - csv file with two columns format <eventID>,<target label>
+    """
+   
+    if overwrite:
+        treg2 = treg
+    else:
+        treg2 = treg.replace('.hdf5','_up.hdf5')
+    cat = pickle.load(open(cat_pickle_filename, 'rb'))
+    dct = {key:idx for idx, key in enumerate(cat.data['eventID'])}
+    
+    idx = []
+    targ = []
+    for line in open(eqlist,'r'):
+        eqid, target = line.split(',')
+        idx.append(dct[eqid])
+        targ.append(target)
+
+    f = h5py.File(treg, "r")
+    f2 = h5py.File(treg2, "w")
+    for key in f.keys():
+        tmp = f[key][:]
+        for eid,tar in zip(idx,targ):
+            if re.search(key, tar):
+                tmp[eid] = True
+            else:
+                tmp[eid] = False
+        f2[key] = tmp
+    f.close()
+    f2.close()
+
+def main(argv):
+    """
+    """
+    p = sap.Script(change)
+    p.arg(name='cat_pickle_filename', help='pickled catalogue')
+    p.arg(name='treg', help='TR hdf5 filename')
+    msg = 'list of events to change. format <eventID>,<target class>'
+    p.arg(name='eqlist', help=msg)
+
+    if len(argv) < 3:
+        print(p.help())
+    else:
+        p.callfunc()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
