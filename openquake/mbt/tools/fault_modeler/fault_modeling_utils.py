6f10d1b691c4ca4f6e3734c8ecc8f11d2e3b8820
klunk386 <poggi.valerio@gmail.com>
2018-10-11 08:56:26 +0200

    removed Anticlne type

--- openquake/mbt/tools/fault_modeler/fault_modeling_utils.py
+++ openquake/mbt/tools/fault_modeler/fault_modeling_utils.py
@@ -655,8 +655,7 @@ rake_map = {'Normal': -90.,
             'Strike-Slip': 0.,
             'Thrust': 90.,
             'Blind-Thrust': 90.,
-            'Spreading-Ridge': -90.,
-            'Anticline': 90.}
+            'Spreading-Ridge': -90.}
 
 dip_map = {'Normal': 60.,
            'Normal-Dextral': 65.,
@@ -673,8 +672,7 @@ dip_map = {'Normal': 60.,
            'Strike-Slip': 90.,
            'Thrust': 40.,
            'Blind-Thrust': 40.,
-           'Spreading-Ridge': 60.,
-           'Anticline': 40.}
+           'Spreading-Ridge': 60.}
 
 # To transform literal values into numbers
 direction_map = {'N': 0.,
@@ -2031,7 +2029,7 @@ def calc_fault_width_from_length(
 WIDTH_CLASS = {'cl1': ['Normal', 'Reverse', 'Thrust', 'Normal-Dextral',
                        'Normal-Sinistral', 'Reverse-Sinistral',
                        'Reverse-Dextral', 'Spreading-Ridge',
-                       'Blind-Thrust', 'Anticline'],
+                       'Blind-Thrust'],
                'cl2': ['Dextral', 'Sinistral', 'Strike-Slip',
                        'Dextral-Normal', 'Dextral-Reverse',
                        'Sinistral-Normal', 'Sinistral-Reverse']
