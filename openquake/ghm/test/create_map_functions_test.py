88ce4dc24e61af117eb7311556dba7d090c4e2e5
Marco <pgnmrc69@gmail.com>
2019-01-29 11:57:59 +0100

    Adding more checks to the homogenisation code

---
+++ openquake/ghm/test/create_map_functions_test.py
@@ -0,0 +1,25 @@
+"""
+Module create_map_functions_test
+"""
+
+import os
+import unittest
+import geopandas as gpd
+
+from openquake.ghm.create_map_from_curves import get_hcurves_geodataframe
+
+DATA = os.path.join(os.path.dirname(__file__), 'data', 'hazardcurve')
+
+
+class ReadHazardCurveTestCase(unittest.TestCase):
+    """ Check information read from hazard curve file """
+
+    def test_read_file_search(self):
+        """ Testing homogenisation between models"""
+        fname = os.path.join(DATA, 'hazard_curve-mean-PGA_18150.csv')
+        gdf, header = get_hcurves_geodataframe(fname)
+        self.assertTrue(header[0] == 'mean', 'Header not matching')
+        self.assertTrue(header[1] == 1.0, 'Header not matching')
+        self.assertTrue(header[2] == 'PGA', 'Header not matching')
+        msg = 'The output type is not a GeoDataFrame'
+        self.assertTrue(isinstance(gdf, gpd.GeoDataFrame), msg)
