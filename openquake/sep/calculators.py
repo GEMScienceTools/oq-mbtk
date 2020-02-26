1752026f15af49ffec75c56dc5dc190e4fbb9615
Richard Styron <richard.h.styron@gmail.com>
2020-02-25 16:02:42 -0800

    working newmark calculators

---
+++ openquake/sep/calculators.py
@@ -0,0 +1,96 @@
+from typing import Optional, Union
+
+import osgeo
+import xarray as xr
+import numpy as np
+from osgeo import gdal
+
+
+from .landslide.common import (
+    static_factor_of_safety,
+    rock_slope_static_factor_of_safety,
+)
+from .landslide.newmark import (
+    newmark_critical_accel,
+    newmark_displ_from_pga_M,
+    prob_failure_given_displacement,
+)
+from .landslide.rotational import rotational_critical_accel
+
+
+def calc_newmark_soil_slide_single_event(
+    pga: Union[float, np.ndarray],
+    M: float,
+    slope: Union[float, np.ndarray],
+    cohesion: Union[float, np.ndarray],
+    friction_angle: Union[float, np.ndarray],
+    saturation_coeff: Union[float, np.ndarray] = 0.1,
+    slab_thickness: Union[float, np.ndarray] = 2.5,
+    soil_dry_density: Union[float, np.ndarray] = 1500.0,
+    water_density=1000.0,
+) -> Union[float, np.ndarray]:
+    """
+    """
+
+    fs = static_factor_of_safety(
+        slope,
+        cohesion,
+        friction_angle,
+        saturation_coeff,
+        slab_thickness,
+        soil_dry_density,
+        water_density,
+    )
+
+    ca = newmark_critical_accel(fs, slope)
+
+    Dn = newmark_displ_from_pga_M(pga, ca, M)
+
+    return Dn
+
+
+def calc_newmark_soil_slide_event_set(
+    pga: Union[float, np.ndarray],
+    M: float,
+    slope: Union[float, np.ndarray],
+    cohesion: Union[float, np.ndarray],
+    friction_angle: Union[float, np.ndarray],
+    saturation_coeff: Union[float, np.ndarray] = 0.1,
+    slab_thickness: Union[float, np.ndarray] = 2.5,
+    soil_dry_density: Union[float, np.ndarray] = 1500.0,
+    water_density=1000.0,
+) -> Union[float, np.ndarray]:
+    """
+    """
+    fs = static_factor_of_safety(
+        slope,
+        cohesion,
+        friction_angle,
+        saturation_coeff,
+        slab_thickness,
+        soil_dry_density,
+        water_density,
+    )
+
+    ca = newmark_critical_accel(fs, slope)
+
+    if isinstance(pga, xr.Dataset):
+        Dn = xr.Dataset(
+            {
+                k: newmark_displ_from_pga_M(da, ca, da.attrs["mag"])
+                for k, da in pga.data_vars.items()
+            }
+        )
+
+    # Dn = newmark_displ_from_pga_M(pga, ca, M)
+
+    return Dn
+
+
+def calc_rock_slope_failures():
+    pass
+
+
+def calc_rotational_failures():
+    pass
+
--- openquake/sep/landslide/common.py
+++ openquake/sep/landslide/common.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 import numpy as np
+import xarray as xr
 
 
 def static_factor_of_safety(
@@ -61,6 +62,8 @@ def static_factor_of_safety(
     if np.isscalar(slope):
         if slope == 0.0:
             slope = 1e-5
+    elif isinstance(slope, xr.DataArray):
+        slope = xr.where(slope == 0.0, 1e-5, slope)
     else:
         slope[slope == 0.0] = 1e-5
 
@@ -74,3 +77,57 @@ def static_factor_of_safety(
     )
 
     return term_1 + term_2 - term_3
+
+
+def rock_slope_static_factor_of_safety(
+    slope: Union[float, np.ndarray],
+    cohesion: Union[float, np.ndarray],
+    friction_angle: Union[float, np.ndarray],
+    relief: Union[float, np.ndarray],
+    rock_dry_density: Union[float, np.ndarray] = 2600.0,
+    root_cohesion: Union[float, np.ndarray] = 0.0,
+) -> Union[float, np.ndarray]:
+    """
+    Static factor of safety for rock-slope failures, from Grant et al. 2016,
+    Engineering Geology.
+
+    This calculation can be done for a scalar or for some mix of scalars and
+    arrays of a constant size. For example, if a regional model is made with a
+    slope array derived from a DEM, other spatially-variable paramters (such as
+    cohesion) should be made into rasters with the same size and resolution.
+
+    :param slope: This is either a scalar or array representing the slope of
+        topography at the point of interest, in degrees.
+
+    :param cohesion: This parameter is the unstressed tensile strength of the
+        material. It should be given in Pa. Typical values are around 20e3 Pa
+        (20 kPa) for soils up to 10e6 (20 MPa) for unfaulted rock, i.e. granite.
+
+    :param friction_angle: The internal friction angle measures the orientation
+        of shear fractures within a substance relative to the principal
+        compressive stress. It should be given in degrees. Typical values are
+        30-40 degrees.
+    
+    :param relief:
+
+    :param rock_dry_density: Density of the drained soil or rock that would make
+        up the slide, in kg/m^3. Many soils are ~1500 kg/m^3, while rock is
+        2500-3200 kg/m^3.
+
+    :param root_cohesion:
+
+
+
+    """
+    r_slope = np.radians(slope)
+    r_friction_angle = np.radians(friction_angle)
+
+    alpha = (r_slope + r_friction_angle) / 2.0
+    h = 0.25 * relief
+    y = rock_dry_density * 9.81
+
+    term_1 = (2 * (cohesion + root_cohesion) * np.sin(r_slope)) / (
+        y * h * np.sin(r_slope - alpha) * np.sin(alpha)
+    )
+
+    return term_1 + (np.tan(r_friction_angle) / np.tan(alpha))
