3eda73a7d8f10bff0c37fcd80294d2e656495be0
Kendra Johnson <kendra.johnson@globalquakemodel.org>
2020-08-03 14:47:15 -0700

    refactoring the smoothing test and adding one that is not for the IDL

--- openquake/mbt/tests/tools/smooth_test.py
+++ openquake/mbt/tests/tools/smooth_test.py
@@ -8,12 +8,7 @@ from openquake.mbt.tools.smooth import Smoothing
 from openquake.hazardlib.geo.mesh import Mesh
 from openquake.hmtk.parsers.catalogue import CsvCatalogueParser
 
-def setUpMesh():
-    #
-    # set bounds crossing the IDL
-    lons = [-179.5, 179.5, 179.5, -179.5, -179.5]
-    lats = [-15.5, -15.5, -16.0, -16.0, -15.5]
-    #
+def setUpMesh(lons,lats):
     # make the mesh
     points = []
     for lon,lat in zip(lons,lats):
@@ -26,16 +21,78 @@ def setUpMesh():
 
     return new_polygon_mesh
 
+def check_symmetry(mesh,values):
+    # take the mesh row with the median longitude  
+    row = numpy.median(mesh.lats)
+    idx = numpy.where(mesh.lats==row)
 
-class SmoothTestCaseIDL(unittest.TestCase):
-    """
-    """
+    # take the lats, lons, and smooth values from that row
+    lats_row = mesh.lats[idx]
+    lons_row = mesh.lons[idx]
+    vals_row = values[idx]
 
-    def testIDL(self):
+    # find the center: the point with the highest smoothing value
+    center = int(numpy.where(vals_row==max(vals_row))[0])
+
+    # take the smoothed values on either side of the center
+    center_left = center - int(numpy.floor(0.45*len(vals_row)))
+    center_right = center + int(numpy.floor(0.45*len(vals_row)))
+    vals_west = vals_row[center_left:center+1]
+    vals_east = numpy.flip(vals_row[center:center_right+1])
+
+    # check that the smoothing values are approximately symmetrical
+    max_vals = max(vals_row)
+    diff = vals_east-vals_west
+    max_diff = max(abs(diff))
+
+    # compute the largest percentage difference
+    p_diff = max_diff/max_vals*100
+
+    return p_diff
+
+
+class SmoothTestCase(unittest.TestCase):
+    """
+    tests that smoothing values sum to N earthquakes and that 
+    smoothing distribution is symmertric. catalogues use a 
+    single earthquake with latitude assigned the median latitude
+    of the mesh
+    """
+    def test_01(self):
         """
+        """
+        # create the mesh
+        # set bounds crossing the IDL
+        lons = [150, 151, 151, 150, 150]
+        lats = [-15.5, -15.5, -16.0, -16.0, -15.5]
+        mesh = setUpMesh(lons,lats)
+
+        # read in the test catalogue
+        cat_filename = '../data/tools/test_catalogue.csv'
+        catalogue_parser = CsvCatalogueParser(cat_filename)
+        cat = catalogue_parser.read_file()
+
+        # smooth the catalogue onto the mesh grid
+        smooth = Smoothing(cat, mesh, 20)
+        values = smooth.gaussian(50, 20)
+
+        # check that smoothed values sum to 1.0
+        self.assertAlmostEqual(sum(values),len(cat.data['depth']),5)
+
+        # check that the Gaussian distribution works across IDL:
+        # assert that max %-difference is < 1
+        self.assertLess(check_symmetry(mesh,values),1)
+
+
+    def test_IDL_02(self):
+        """
+        tests that the smoothing works accross the IDL
         """
         # create the mesh 
-        mesh = setUpMesh()
+        # set bounds crossing the IDL
+        lons = [-179.5, 179.5, 179.5, -179.5, -179.5]
+        lats = [-15.5, -15.5, -16.0, -16.0, -15.5]
+        mesh = setUpMesh(lons,lats)
 
         # read in the test catalogue
         cat_filename = '../data/tools/idl_test_catalogue.csv' 
@@ -49,34 +106,7 @@ class SmoothTestCaseIDL(unittest.TestCase):
         # check that smoothed values sum to 1.0
         self.assertAlmostEqual(sum(values),len(cat.data['depth']),5)
 
-        # check that the Gaussian distribution works across IDL
-
-        # take the mesh row with the median longitude  
-        row = numpy.median(mesh.lats)
-        idx = numpy.where(mesh.lats==row)
-
-        # take the lats, lons, and smooth values from that row
-        lats_row = mesh.lats[idx]
-        lons_row = mesh.lons[idx]
-        vals_row = values[idx]
-
-        # find the center: the point with the highest smoothing value
-        center = int(numpy.where(vals_row==max(vals_row))[0])
-
-        # take the smoothed values on either side of the center
-        center_left = center - int(numpy.floor(0.45*len(vals_row)))
-        center_right = center + int(numpy.floor(0.45*len(vals_row)))
-        vals_west = vals_row[center_left:center+1]
-        vals_east = numpy.flip(vals_row[center:center_right+1])
-
-        # check that the smoothing values are approximately symmetrical
-        max_vals = max(vals_row)
-        diff = vals_east-vals_west
-        max_diff = max(abs(diff))
-        
-        # compute the largest percentage difference
-        p_diff = max_diff/max_vals*100
-
+        # check that the Gaussian distribution works across IDL:
         # assert that max %-difference is < 1
-        self.assertLess(p_diff,1)
+        self.assertLess(check_symmetry(mesh,values),1)
 
