070bee5c8f6e84cc2350771bb0b4efa286fb3b10
Michele Simionato <michele.simionato@gmail.com>
2020-08-20 15:18:46 +0200

    Fixed test_create_01

--- openquake/mbt/tests/notebooks/project/create_project_test.py
+++ openquake/mbt/tests/notebooks/project/create_project_test.py
@@ -1,4 +1,3 @@
-
 import os
 import unittest
 
@@ -13,22 +12,20 @@ class TestCreateProject(unittest.TestCase):
         """
         Create an oqmbt project
         """
-        # MN: 'filename' assigned but never used
-        inifile = filename = os.path.join(self.BASE_DATA_PATH,
-                                          'project.ini')
+        inifile = os.path.join(self.BASE_DATA_PATH, 'project.ini')
         argv = [inifile]
         project_create(argv)
-        #
+
         # files created with the construction of the project
         fles = ['eqk_rates.hdf5', 'hypo_close_to_faults.hdf5', 'test.oqmbtp',
                 'hypo_depths.hdf5', 'completeness.hdf5']
         #
         # checking files existence
-        folder = './../../tmp/project_test'
+        folder = '/tmp/project_test'
         for fle in fles:
             tmp = os.path.join(folder, fle)
             print(tmp)
             assert os.path.isfile(tmp)
-        #
+
         # check that in the new folder there are no other files
         self.assertEqual(len(os.walk(folder).__next__()[2]), 5)
