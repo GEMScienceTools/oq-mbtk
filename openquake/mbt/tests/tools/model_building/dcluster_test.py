773845823f2a10986e07da08bf3faf7f8047c62d
Michele Simionato <michele.simionato@gmail.com>
2020-08-21 06:02:37 +0200

    Replace nosetest with pytest

--- openquake/mbt/tests/tools/model_building/dcluster_test.py
+++ openquake/mbt/tests/tools/model_building/dcluster_test.py
@@ -1,11 +1,14 @@
 import os
 import shutil
+import pathlib
+import tempfile
 import unittest
 
 from openquake.mbt.tools.model_building.plt_tools import _load_catalogue
 from openquake.mbt.tools.model_building.dclustering import decluster
+from openquake.mbt.tests import __file__ as tests__init__
 
-BASE_PATH = os.path.dirname(__file__)
+TDIR = pathlib.Path(tests__init__).parent
 
 
 class TrTestCase(unittest.TestCase):
@@ -15,29 +18,19 @@ class TrTestCase(unittest.TestCase):
     """
 
     def setUp(self):
-        self.root_folder = os.path.join(BASE_PATH)
-        self.tmp = os.path.join(BASE_PATH, '../../tmp/')
-        #
-        # creating temporary folder
-        if os.path.exists(self.tmp):
-            shutil.rmtree(self.tmp)
-        os.makedirs(self.tmp)
-        #
+        self.tmp = tempfile.mkdtemp()
         # set datasets
-        tmps = '../../data/tools/model_building/dcluster/catalogue.csv'
-        self.catalogue = os.path.abspath(os.path.join(BASE_PATH, tmps))
-        tmps = '../../data/tools/model_building/dcluster/classification.hdf5'
-        self.classification = os.path.join(BASE_PATH, tmps)
+        self.catalogue = str(
+            TDIR / 'data/tools/model_building/dcluster/catalogue.csv')
+        self.classification = (
+            TDIR / 'data/tools/model_building/dcluster/classification.hdf5')
 
     def tearDown(self):
-        #
         # removing tmp folder
-        # shutil.rmtree(self.tmp)
-        pass
+        if os.path.exists(self.tmp):
+            shutil.rmtree(self.tmp)
 
     def testcase01(self):
-        """
-        """
         config = {'time_distance_window': 'GardnerKnopoffWindow',
                   'fs_time_prop': 0.9}
         decluster(catalogue_hmtk_fname=self.catalogue,
@@ -50,20 +43,19 @@ class TrTestCase(unittest.TestCase):
                   format='pkl')
         #
         # Read first mainshock catalogue
-        a_fname = os.path.abspath(os.path.join(
-            './../../tmp/catalogue_dec_a.pkl'))
+        a_fname = os.path.join(self.tmp, 'catalogue_dec_a.pkl')
         self.assertTrue(os.path.exists(a_fname))
         cat = _load_catalogue(a_fname)
         self.assertTrue(len(cat.data['magnitude'] == 1))
         self.assertAlmostEqual(cat.data['magnitude'][0], 6.0)
         #
         # Read second mainshock catalogue
-        b_fname = os.path.join('./../../tmp/catalogue_dec_b.pkl')
+        b_fname = os.path.join(self.tmp, 'catalogue_dec_b.pkl')
         self.assertTrue(os.path.exists(b_fname))
         cat = _load_catalogue(b_fname)
         self.assertTrue(len(cat.data['magnitude'] == 1))
         self.assertAlmostEqual(cat.data['magnitude'][0], 6.1)
         #
         # Check that the third mainshock catalogue does not exist
-        c_fname = os.path.join('./../../tmp/catalogue_dec_c.pkl')
+        c_fname = os.path.join(self.tmp, 'catalogue_dec_c.pkl')
         self.assertFalse(os.path.exists(c_fname))
