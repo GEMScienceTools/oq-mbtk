c107f16b7a76ccdf051bf4ffba9353f50a783dd8
Marco Pagani <marco.pagani@globalquakemodel.org>
2021-06-09 11:39:39 +0200

    First version working!

--- openquake/mbi/__main__.py
+++ openquake/mbi/__main__.py
@@ -6,19 +6,19 @@ import sys
 import logging
 
 from openquake.baselib import sap
-from openquake import cmd
+from openquake import mbi
 
 PY_VER = sys.version_info[:3]
 if PY_VER < (3, 6):
     sys.exit('Python 3.6+ is required, you are using %s', sys.executable)
 
 
-def mbtk():
+def oqm():
     args = set(sys.argv[1:])
     level = logging.DEBUG if 'debug' in args else logging.INFO
     logging.basicConfig(level=level)
-    sap.run(cmd, prog='oqmbtk')
+    sap.run(mbi, prog='oqm')
 
 
 if __name__ == '__main__':
-    mbtk()
+    oqm()
