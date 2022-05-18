019e3ebf62f06abc997f637686f5b3a6fecc775f
Antonio Ettorre <antonio.ettorre@globalquakemodel.org>
2022-05-18 14:54:59 +0200

    add :USERPROFILE to install.ps1

--- windows/oq-console.bat
+++ windows/oq-console.bat
@@ -1,9 +1,11 @@
 @echo off
 setlocal
-set mypath=%~dp0
-set PATH=%mypath%\python3.6;%mypath%\python3.6\Scripts;%PATH%
+
+set mypath=%USERPROFILE%\mbtk
+set PATH=%mypath%;%mypath%\Scripts;%PATH%
 echo OpenQuake environment loaded
 echo To see versions of installed software run 'pip freeze'
 echo To run OpenQuake use 'oq' and 'oq engine'
 cmd /k
+
 endlocal
