03d79c09387a88fb5cf8efb81597b5d0dcabf709
Kendra Johnson <kendra.johnson@globalquakemodel.org>
2022-08-31 11:25:42 +0200

    adding mps

--- openquake/plt/mapping.py
+++ openquake/plt/mapping.py
@@ -269,6 +269,49 @@ class HMTKBaseMap(object):
             self.gmt_files_list.append(filename)
 
 
+    def _plot_multi_point_source(self, source):
+        '''
+        Adds point sources defined as multipoint to mapping script.
+        :param source:
+            point source as instance of
+            :class:`openquake.hazardlib.source.point.MultiPointSource`
+        :param float pointsize:
+            sets the size of plotting symbols
+        '''
+
+        pnum = int(re.sub("[^0-9]", "", self.J))
+        pointsize = 0.01 * pnum
+
+        lons = source.mesh.lons
+        lats = source.mesh.lats
+
+        mfds = [m for m in source.mfd]
+        cmrates = []
+        for mfd in mfds:
+            roc = list(map(list, zip(*mfd.get_annual_occurrence_rates())))
+            cmrates.append(sum(roc[1]))
+
+
+        filename = '{}/mtkMultiPointSource.csv'.format(self.out)
+
+        add_plot_line = self.mk_plt_csv(lons, lats, filename, color_column=cmrates)
+
+        if add_plot_line == 1:
+            # Making cpt
+            self.gmt_files_list.append(filename)
+            cpt_fle = "{}/cf_tmp.cpt".format(self.out)
+            self.gmt_files_list.append(cpt_fle)
+            cbar_int = 0.3*(max(cmrates) - min(cmrates))
+            self.cmds.insert(0,"gmt makecpt -Cjet -T0/{}/30+n > {:s}".format(
+            #self.cmds.insert(0,"gmt makecpt -Cjet -T0/{}/2> {:s}".format(
+                max(cmrates), cpt_fle))
+
+            self.cmds.append('gmt plot {} -C{} -Ss0.075 -t10'.format(filename, cpt_fle))
+            self.cmds.append('gmt colorbar -DJBC -Ba+l"Cumulative rate" -C{}'.format(
+            #self.cmds.append('gmt colorbar -DJBC -Ba{}+l"Cumulative rate" -C{}'.format(
+                cpt_fle))
+                #spacing, cpt_fle))
+
     def _plot_simple_fault(self, source):
         '''
         Adds simple fault sources to mapping script. 
@@ -415,6 +458,8 @@ class HMTKBaseMap(object):
                     self.plot_polygon(source.polygon)
                 elif type(source).__name__ == 'PointSource': 
                     self._plot_point_source(source)
+                elif type(source).__name__ == 'MultiPointSource': 
+                    self._plot_multi_point_source(source)
                 elif type(source).__name__ == 'ComplexFaultSource':
                     self._plot_complex_fault(source)
                 elif type(source).__name__ == 'SimpleFaultSource':
@@ -502,7 +547,7 @@ class HMTKBaseMap(object):
         if sscale is None:
             sz = [coeff] * len(latitude)
         else: 
-            sz = smin + coeff * data ** sscale
+            sz = [smin + coeff * d ** sscale for d in data]
 
         if logplot:
             data = np.log10(data.copy())
