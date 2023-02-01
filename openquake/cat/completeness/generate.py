b8b662ffad34d5657e7232f8b855baf4f7e89a88
Marco Pagani <marco.pagani@globalquakemodel.org>
2023-02-01 15:21:12 +0100

    Adding tests + improving apriori conditions

--- openquake/cat/completeness/generate.py
+++ openquake/cat/completeness/generate.py
@@ -31,6 +31,21 @@ import toml
 import numpy as np
 import multiprocessing
 from openquake.wkf.utils import create_folder
+from openquake.cat.completeness.analysis import clean_completeness
+
+
+def get_compl(perms, mags, years, idxperm, flexible):
+    """
+    example: get_compl(perms, mags, years, 0, True)
+    """
+    if flexible:
+        idx = np.where(perms[idxperm, :] == len(mags) + 1, -1, perms)
+    else:
+        idx = perms[idxperm, :]
+    idx = idx.astype(int)
+    tmp = np.array([(y, m) for y, m in zip(years, mags[idx])])
+    ctab = clean_completeness(tmp)
+    return ctab
 
 
 def mm(a):
@@ -52,23 +67,66 @@ def get_completenesses(fname_config, folder_out):
     years = np.array(config[key]['years'])
     num_steps = config[key].get('num_steps', 0)
     min_mag_compl = config[key].get('min_mag_compl', None)
-    apriori_conditions = config[key].get('apriori_conditions', {})
+    # This is left for back compatibility
+    apriori_conditions_out = config[key].get('apriori_conditions', {})
+    apriori_conditions_out = config[key].get('apriori_conditions_out',
+                                             apriori_conditions_out)
+    apriori_conditions_in = config[key].get('apriori_conditions_in', {})
     step = config[key].get('step', 8)
     flexible = config[key].get('flexible', False)
 
     _get_completenesses(mags, years, folder_out, num_steps, min_mag_compl,
-                        apriori_conditions, step, flexible)
+                        apriori_conditions_out, apriori_conditions_in, step,
+                        flexible)
 
 
 def _get_completenesses(mags, years, folder_out=None, num_steps=0,
-                        min_mag_compl=None, apriori_conditions={},
-                        step=6, flexible=False):
+                        min_mag_compl=None, apriori_conditions_out={},
+                        apriori_conditions_in={}, step=6, flexible=False):
     """
+    This function generates a set of completeness windows starting from a
+    set of magnitude and time thresholds included in the `mags` and `years`
+    vectors.
+
+    The output is a triple
+
     :param mags:
         A list or numpy array in increasing order
     :param years:
         A list or numpy array in increasing order
+    :param folder_out:
+        A string with the output folder. When `None` the code does not save
+        the produced output.
+    :param num_steps:
+        The minimum number of steps that each completeness window must have.
+        Zero corresponds to a flat completeness window.
+    :param min_mag_compl:
+        The minimum value of magnitude used to define the completeness. Note
+        that this is also the minimum magnitude that will be used to compute
+        the magnitude-frequency distribution.
+    :param apriori_conditions:
+        This is a dictionary where keys are years and values are magnitudes.
+        Each year-magnitude combination defines points (i.e. earthquakes)
+        that must be left out by the selected completeness windows.
+    :param step:
+        The step used to parallelize the calculation [default is 6]
+    :param flexible:
+        A boolean. When False the completeness window is always starting on
+        the right with the smallest magnitude value
+    :returns:
+        A triple of :class:`numpy.ndarray` instances. The first one, `perms`,
+        is an array of size <number of perms> x <number of time intervals>.
+        The array contains integer indexes indicating the magnitude value
+        of completeness for the corresponding time period. For example,
+        if `years` is [1900, 2000], `mags` is [3, 4, 5], the row of
+        perms corresponding to [0, 2], states that the catalogue is complete
+        above 5 since 1900 and above 3 since 2000.
     """
+
+    if isinstance(mags, list):
+        mags = np.array(mags)
+    if isinstance(years, list):
+        years = np.array(years)
 
     msg = 'Years must be in ascending order'
     assert np.all(np.diff(years) > 0), msg
@@ -87,16 +145,18 @@ def _get_completenesses(mags, years, folder_out=None, num_steps=0,
     if min_mag_compl is None:
         min_mag_compl = min(mags)
 
+    # Check if the catalogue contains magnitudes above the minimum threshold
     if len(np.where(min_mag_compl <= mags)) < 1:
         msg = 'None of the magnitude intervals above the min_mag_compl'
         raise ValueError(msg)
     max_first_idx = np.min(np.where(min_mag_compl <= mags))
 
     # Info
-    print('Total number of combinations : {:,d}'.format(len(mags)**len(years)))
-    print(f'Index of first magnitude     : {max_first_idx:,d}')
+    tmp = len(mags)**len(years)
+    print('Total number of combinations                : {:,d}'.format(tmp))
+    print(f'Index of the min magnitude of completeness  : {max_first_idx:,d}')
 
-    # Creating the possible completenesses
+    # Creating the all the possible completenes windows
     perms = []
     for y in [years[i:min(i+step, len(years))] for i in range(0, len(years),
                                                               step)]:
@@ -108,11 +168,7 @@ def _get_completenesses(mags, years, folder_out=None, num_steps=0,
             # with time
             p = p[np.diff(p, axis=1).min(axis=1) >= 0, :]
 
-            # Selecting combinations within min_mag_compl
-            # if max(years) in y:
-            #     p = p[p[:, 0] <= max_first_idx, :]
-
-            # Updating
+            # Update the list with the permutations
             if len(perms):
                 new = []
                 for x in perms:
@@ -122,7 +178,7 @@ def _get_completenesses(mags, years, folder_out=None, num_steps=0,
             else:
                 perms = p
 
-    # Full set of possible completeness windows. Each perm in perms is a
+    # Full set of possible completeness windows. Each perm in `perms` is a
     # list of indexes for the magnitudes in the mags vector (in increasing
     # order), starting from the most recent time interval. So 0 in the
     # first position means the lowest value of magnitude for the most
@@ -142,24 +198,30 @@ def _get_completenesses(mags, years, folder_out=None, num_steps=0,
     # lower or equal than a threshold
     perms = perms[perms[:, 0] <= max_first_idx, :]
 
-    # Applying a-priori conditions
-    for yea_str in apriori_conditions.keys():
-        yea = float(yea_str)
-        mag = float(apriori_conditions[yea_str])
-        idx_yea = np.minimum(np.max(np.where(years >= yea)), len(years)-1)
-        idx_mag = np.max(np.where(mag >= mags))
-        # idxs = np.minimum(perms[:, idx_yea], len(mags)-1-dlt)
-        # perms = perms[idxs <= idx_mag, :]
-        perms = perms[perms[:, idx_yea] <= idx_mag, :]
-
-    print(f'Total number selected        : {len(perms):,d}')
-
     # Replacing the index of the 'buffer;' magnitude
     if flexible:
         perms = np.where(perms == len(mags), -1, perms)
 
+    # Applying a-priori conditions
+    for yea_str in apriori_conditions_out.keys():
+        yea = float(yea_str)
+        mag = float(apriori_conditions_out[yea_str])
+        idx_yea = np.min(np.where(yea >= years))
+        idx_mag = np.min(np.where(mag < mags))
+        perms = perms[perms[:, idx_yea] >= idx_mag, :]
+
+    # Applying a-priori conditions IN
+    for yea_str in apriori_conditions_in.keys():
+        yea = float(yea_str)
+        mag = float(apriori_conditions_in[yea_str])
+        idx_yea = np.min(np.where(yea >= years))
+        idx_mag = np.max(np.where(mags > mag))
+        perms = perms[perms[:, idx_yea] < idx_mag, :]
+
+    print(f'Total number selected completeness windows  : {len(perms):,d}')
+
     if folder_out is not None:
-        print(f'Saving completeness tables in: {folder_out:s}')
+        print(f'Saving completeness tables in               : {folder_out:s}')
         np.save(os.path.join(folder_out, 'dispositions.npy'), perms)
         np.save(os.path.join(folder_out, 'mags.npy'), mags)
         np.save(os.path.join(folder_out, 'years.npy'), years)
--- openquake/cat/tests/completeness/generate_test.py
+++ openquake/cat/tests/completeness/generate_test.py
@@ -26,44 +26,151 @@
 
 import unittest
 import numpy as np
-from openquake.hmtk.seismicity.catalogue import Catalogue
-from openquake.hmtk.seismicity.occurrence.utils import get_completeness_counts
-from openquake.mbt.tools.model_building.dclustering import _add_defaults
-from openquake.cat.completeness.norms import get_completeness_matrix
+from openquake.cat.completeness.generate import _get_completenesses
+from openquake.cat.completeness.plot import plot_completeness
+
+PLOT = False
 
 
-class CompletenessMatrixTest(unittest.TestCase):
+class GenerateTest(unittest.TestCase):
+    """
+    Test the generation of sets of completeness windows
+    """
 
-    def setUp(self):
-        dat = [[1900, 6.0],
-               [1980, 6.0],
-               [1970, 5.0],
-               [1980, 5.0],
-               [1990, 5.0]]
-        dat = np.array(dat)
-        cat = Catalogue()
-        cat.load_from_array(['year', 'magnitude'], dat)
-        cat = _add_defaults(cat)
-        cat.data["dtime"] = cat.get_decimal_time()
-        self.cat = cat
-        self.compl = np.array([[1980, 5.0], [1950, 5.9]])
+    def test_generate_simple(self):
 
-    def test_case01(self):
-        binw = 0.5
-        oin, out, cmags, cyeas = get_completeness_matrix(self.cat, self.compl,
-                                                         0.5, 10.0)
-        oin_expected = np.array([[-1., -1., -1., -1., -1., -1., -1., -1.,  1.,  1.],
-                                 [-1., -1., -1., -1., -1.,  0.,  0.,  0.,  0.,  0.],
-                                 [-1., -1., -1., -1., -1.,  0.,  0.,  0.,  1.,  0.]])
-        out_expected = np.array([[ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  1., -1., -1.],
-                                 [ 0.,  0.,  0.,  0.,  0., -1., -1., -1., -1., -1.],
-                                 [ 1.,  0.,  0.,  0.,  0., -1., -1., -1., -1., -1.]])
-        np.testing.assert_array_equal(oin, oin_expected)
-        np.testing.assert_array_equal(out, out_expected)
+        mags = [4.0, 5.0, 6.0]
+        years =  [1980, 1990, 2000]
+        num_steps = 0
+        min_mag_compl = 4.0
+        apriori_conditions_out = {}
+        apriori_conditions_in = {}
+        step = 6
+        flexible = False
 
-        # Check the consistency of results with the ones provided by the
-        # completeness count
-        cmag, t_per, n_obs = get_completeness_counts(self.cat, self.compl,
-                                                     binw)
-        oin[oin < 0] = 0.0
-        np.testing.assert_array_equal(np.sum(oin, axis=1), n_obs)
+        # Generate the set of completeness windows
+        perms, mags, years = _get_completenesses(
+            mags, years, folder_out=None, num_steps=num_steps,
+            min_mag_compl=min_mag_compl,
+            apriori_conditions_out=apriori_conditions_out,
+            apriori_conditions_in=apriori_conditions_in,
+            step=step, flexible=flexible)
+
+        expected = np.array([[0, 2, 2],
+                             [0, 1, 2],
+                             [0, 1, 1],
+                             [0, 0, 2],
+                             [0, 0, 1],
+                             [0, 0, 0]])
+
+        # Test
+        np.testing.assert_equal(perms, expected)
+
+        if PLOT:
+            plot_completeness(perms, mags, years, 1980, 2005, 3.9, 6.1, '')
+
+    def test_generate_flexible(self):
+        """ test flexible is True """
+        # As the previous test but with flexible = True
+
+        mags = [4.0, 5.0, 6.0]
+        years =  [1980, 1990, 2000]
+        num_steps = 0
+        min_mag_compl = 4.0
+        apriori_conditions_out = {}
+        apriori_conditions_in = {}
+        step = 6
+        flexible = True
+
+        # Generate the set of completeness windows. The `perms` matrix in this
+        # case has a length of 10 rather than 6 as in the previous case.
+        perms, mags, years = _get_completenesses(
+            mags, years, folder_out=None, num_steps=num_steps,
+            min_mag_compl=min_mag_compl,
+            apriori_conditions_out=apriori_conditions_out,
+            apriori_conditions_in=apriori_conditions_in,
+            step=step, flexible=flexible)
+
+        expected = np.array([[ 0, -1, -1],
+                             [ 0,  2, -1],
+                             [ 0,  2,  2],
+                             [ 0,  1, -1],
+                             [ 0,  1,  2],
+                             [ 0,  1,  1],
+                             [ 0,  0, -1],
+                             [ 0,  0,  2],
+                             [ 0,  0,  1],
+                             [ 0,  0,  0]])
+
+        # Test
+        np.testing.assert_equal(perms, expected)
+
+        if PLOT:
+            plot_completeness(perms, mags, years, 1980, 2005, 3.9, 6.1, '')
+
+
+    def test_generate_apriori(self):
+        """ test use of apriori conditions """
+        # As the previous test but with two apriori conditions
+
+        mags = [4.0, 5.0, 6.0]
+        years =  [1980, 1990, 2000, 2010]
+        num_steps = 0
+        min_mag_compl = 4.0
+        apriori_conditions_out = {1985: 5.5, 1995: 4.5}
+        apriori_conditions_in = {}
+        step = 6
+        flexible = True
+
+        # Generate the set of completeness windows. The `perms` matrix in this
+        # case has a length of 10 rather than 6 as in the previous case.
+        perms, mags, years = _get_completenesses(
+            mags, years, folder_out=None, num_steps=num_steps,
+            min_mag_compl=min_mag_compl,
+            apriori_conditions_out=apriori_conditions_out,
+            apriori_conditions_in=apriori_conditions_in,
+            step=step, flexible=flexible)
+
+        expected = np.array([[0, 2, 2, 2],
+                             [0, 1, 2, 2],
+                             [0, 1, 1, 2],
+                             [0, 0, 2, 2],
+                             [0, 0, 1, 2]])
+
+        if PLOT:
+            plot_completeness(perms, mags, years, 1980, 2005, 3.9, 6.1, '',
+                              apriori_conditions_out)
+
+        # Test
+        np.testing.assert_equal(perms, expected)
+
+    def test_generate_apriori_in_out(self):
+        """ test use of apriori conditions in/out"""
+        # As the previous test but also with apriori conditions in
+
+        mags = [4.0, 5.0, 6.0]
+        years =  [1980, 1990, 2000, 2010]
+        num_steps = 0
+        min_mag_compl = 4.0
+        apriori_conditions_out = {1985: 5.5, 1995: 4.5}
+        apriori_conditions_in = {1995: 5.5}
+        step = 6
+        flexible = True
+
+        # Generate the set of completeness windows. The `perms` matrix in this
+        # case has a length of 10 rather than 6 as in the previous case.
+        perms, mags, years = _get_completenesses(
+            mags, years, folder_out=None, num_steps=num_steps,
+            min_mag_compl=min_mag_compl,
+            apriori_conditions_out=apriori_conditions_out,
+            apriori_conditions_in=apriori_conditions_in,
+            step=step, flexible=flexible)
+
+        expected = np.array([[0, 1, 1, 2], [0, 0, 1, 2]])
+
+        if PLOT:
+            plot_completeness(perms, mags, years, 1980, 2005, 3.9, 6.1, '',
+                              apriori_conditions_out, apriori_conditions_in)
+
+        # Test
+        np.testing.assert_equal(perms, expected)
