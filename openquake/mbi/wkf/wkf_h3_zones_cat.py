f8275028d670dfc731e078d6987df706049e45fd
Kirsty Bayliss <kirsty.bayliss@globalquakemodel.org>
2023-04-06 11:59:51 +0200

    Changes to adaptive smoothing workflow to work with specific zones when required

--- openquake/mbi/wkf/wkf_h3_zones_cat.py
+++ openquake/mbi/wkf/wkf_h3_zones_cat.py
@@ -9,8 +9,9 @@ import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point, Polygon, LineString
 from openquake.baselib import sap
+from openquake.wkf.utils import get_list
 
-def main(h3_level: str, fname_catalogue : str, fname_out: str):
+def main(h3_level: str, fname_catalogue : str, fname_out: str, poly_file: str, use: str = []):
     '''
     Construct H3 zones covering area of extent of catalogue. ToDo: Map cells to source zones. 
     
@@ -27,25 +28,34 @@ def main(h3_level: str, fname_catalogue : str, fname_out: str):
     df = pd.read_csv(fname_catalogue)
     catalogue = gpd.GeoDataFrame(df, crs='epsg:4326', geometry=[Point(xy) for xy
                            in zip(df.longitude, df.latitude)])
+                           
+    # Read polygons from a file where this is provided
+    # Best practice would be to provide this! 
+    # Yeah this is exactly what Marco's code does you fool!
+    if poly_file:
+        polygons_gdf = gpd.read_file(poly_file)
+    else:
+        # Make boundary polygon from catalogue extent                       
+        xmin,ymin,xmax,ymax  = catalogue.total_bounds
+        polygon = shapely.geometry.Polygon([(xmin, ymin),  (xmax, ymin), (xmax, ymax), (xmin, ymax),  (xmin, ymin)])
+        # Make polygon into geopandas dataframe
+        gdf = gpd.GeoDataFrame(index=[0], crs='epsg:4326', geometry=[polygon])
+        gdf['id'] = 1
     
-    # Make boundary polygon from catalogue extent                       
-    xmin,ymin,xmax,ymax  = catalogue.total_bounds
-    polygon = shapely.geometry.Polygon([(xmin, ymin),  (xmax, ymin), (xmax, ymax), (xmin, ymax),  (xmin, ymin)])
-
-    gojpol = eval(json.dumps(shapely.geometry.mapping(polygon)))
-    
-    # Revert the positions of lons and lats
-    coo = [[c[1], c[0]] for c in gojpol['coordinates'][0]]
-    gojpol['coordinates'] = [coo]
-    
-    # Generate h3 hexagons over the area, save in dataframe
-    hexagons = list(h3.polyfill(gojpol, h3_level))
-    
+    # Only calculate for spatial polygon ids in use list     
+    if len(use) > 0: 
+        use = get_list(use)
+        use = map(int, use)
+        polygons_gdf = polygons_gdf[polygons_gdf['id'].isin(use)]
+    # Generate h3 hexagons over the area, save in dataframe 
     fout = open(fname_out, 'w')
-    for hxg in hexagons:
-        # TODO: Update this to take correct source zone names from source zone polygons
-        zid = 1
-        fout.write("{:s},{:d}\n".format(hxg, zid))
+    print(len(polygons_gdf))
+    for index,row in polygons_gdf.iterrows():
+        hexagons = h3.polyfill(row['geometry'].__geo_interface__, h3_level, geo_json_conformant=True)
+    
+        for hxg in hexagons:
+            fout.write("{:s},{:d}\n".format(hxg, row['id']))    
+
 
 main.h3_level = "h3 resolution level"
 main.fname_catalogue = "file containing an earthquake catalogue in .csv format"
