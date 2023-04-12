a16af4f72803d29b627a607036846cbc2587688b
unknown <christopher.brooks@globalquakemodel.org>
2023-04-12 17:18:09 +0200

    Improved version of al-atik implementation. This version cannot currently implement ESHM20 K20 or NGAEast, but retains the speed of calculation for residuals (my method for implementing K20 ESHM20 and NGAEast was very slow because was effectively recreating context for each residual each time). Some small additional improvements prior to review should allow implementation of these additional GMPEs in a more efficient way than my previous PR. Also some general improvements to plots when handling many inputs from .toml

--- openquake/smt/tests/sm_utils_test.py
+++ openquake/smt/tests/sm_utils_test.py
@@ -29,6 +29,8 @@ from openquake.smt.sm_utils import convert_accel_units, SCALAR_XY, al_atik_sigma
 from openquake.hazardlib.gsim.mgmpe import modifiable_gmpe as mgmpe
 
 
+BASE_DATA_PATH = os.path.join(os.path.dirname(__file__))
+
 # OLD IMPLEMENTATION OF CONVERT ACCELERATION UNITS. USED HERE
 # TO COMPARE RESULTS
 def convert_accel_units_old(acceleration, units):
@@ -143,29 +145,15 @@ class SmUtilsTestCase(unittest.TestCase):
 
     def test_al_atik_sigma_check(self):
         """
-        Check that sigma for GMPEs inputted from both gmpe_list and .toml file
-        are checked correctly, and that Al-Atik (2015) sigma model is correctly
-        implemented if required
+        Check that sigma for GMPEs is checked correctly, and that Al-Atik (2015)
+        sigma model is correctly implemented if required
         """
-        # If from gmpe_list, only YA15 should be flagged, but no sigma model
-        # should be implemented (must be specified in input .toml)
-        gmpe_list = ['YenierAtkinson2015BSSA', 'BooreEtAl2014']
-        imts = ['PGA']
-        for idx, inputted_gmpe in enumerate(gmpe_list):
-            gmpe_outputted, gmpe_sigma_flag = al_atik_sigma_check(inputted_gmpe,
-                                                                  imts[0], task
-                                                                  = 'residual')
-            if idx == 0:
-                self.assertTrue(gmpe_sigma_flag == True) # Check flagged
-                self.assertTrue(gmpe_outputted == valid.gsim(inputted_gmpe)) # Check not modified GMPE
-            if idx == 1:
-                self.assertTrue(gmpe_sigma_flag == False) # Check not flagged
-                self.assertTrue(gmpe_outputted == valid.gsim(inputted_gmpe)) # Check not modified GMPE
-                
+
         # If from .toml only YA15 should be flagged and Al-Atik 2015 added
-        BASE_DATA_PATH = os.path.join(os.path.dirname(__file__))
         filename = os.path.join(BASE_DATA_PATH, 'sm_utils_test.toml')
         residuals = res.Residuals.from_toml(filename)
+        
+        imts = ['PGA']
         
         for idx, inputted_gmpe in enumerate(residuals.gmpe_list):
             gmpe_outputted, gmpe_sigma_flag = al_atik_sigma_check(inputted_gmpe,
