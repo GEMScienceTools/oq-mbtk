cfa89913a821dae9d344f0380c07cdbfbf203816
Hazard Team <hazard@p710-oq-1.gem.lan>
2023-04-14 20:32:14 +0200

    modified the scripts ofr vs30 calc

--- openquake/ghm/bin/create_curves.sh
+++ openquake/ghm/bin/create_curves.sh
@@ -31,13 +31,15 @@ IMTSTR='PGA'
 I_SHP='./../data/gis/inland.shp'
 #
 BUF=50.0
+#flag set to 1 for vs30 calc
+VS30_FLAG=1
 #
 # List of models to be processed. This is an optional parameter. If not set,
 # i.e. MDLS="", all the models specified in `openquake.ghm.mosaic.DATA`
 # will be used.
 #MDLS="-m als,arb,aus,cca,cea,chn,eur,gld,haw,idn,ind,jpn,kor,mex,mie,naf,nea,nwa,nzl,pac,phl,png,sam,sea,ssa,twn,waf,zaf"
 #MDLS="-m usa,cnd"
-#MDLS="-m cca,sam"
+#MDLS="-m waf,ssa"
 #
 # Run hazard curves homogenisation
-../create_homogenised_curves.py $C_SHP $O_PATH $D_PATH $SIDX $B_SHP $IMTSTR $I_SHP $BUF $MDLS
+../create_homogenised_curves.py $C_SHP $O_PATH $D_PATH $SIDX $B_SHP $IMTSTR $I_SHP $BUF $MDLS $VS30_FLAG
