979e70e1b00927ece78ecbb4d0fa73aae4e9f6ce
Kendra Johnson <kendra.johnson@globalquakemodel.org>
2023-05-08 12:24:14 +0200

    using engine test syntax

--- openquake/man/tests/tools/csv_output_test.py
+++ openquake/man/tests/tools/csv_output_test.py
@@ -51,13 +51,14 @@ class TestOutputFormat(CalculatorTestCase):
         """
         will fail if the output format changes
         """
-        BASE_CASE8 = os.path.join(os.path.dirname(__file__), 'case_8')
+        #BASE_CASE8 = os.path.join(os.path.dirname(__file__), 'case_8')
         # run test job
-        self.run_calc('',  'case_8/job.ini')
-        #self.run_calc(case_8.__file__,  'job.ini')
+#        self.run_calc('',  'case_8/job.ini')
+        self.run_calc(case_8.__file__,  'job.ini')
         # test mre results output format
         [fname] = export(('disagg-stats', 'csv'), self.calc.datastore)
-        self.assertEqualFiles(os.path.join(BASE_CASE8, 'expected/Mag_Dist_Eps-mean-0.csv'), fname)
+        self.assertEqualFiles('expected/Mag_Dist_Eps-mean-0.csv', fname)
+        #self.assertEqualFiles(os.path.join(BASE_CASE8, 'expected/Mag_Dist_Eps-mean-0.csv'), fname)
 
 
 class TestMDeOutput(unittest.TestCase):
