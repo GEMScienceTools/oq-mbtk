245408bb124f867b0a29b89822e9d49b78174d2f
Kirsty Bayliss <kirsty.bayliss@globalquakemodel.org>
2023-05-24 10:20:48 +0200

    fixes to test

--- openquake/sub/tests/create_profiles_from_slab2pt0_test.py
+++ openquake/sub/tests/create_profiles_from_slab2pt0_test.py
@@ -30,7 +30,10 @@ import unittest
 import numpy as np
 from openquake.sub.get_profiles_from_slab2pt0 import (get_profiles,
                                                       get_bounding_box,
-                                                      get_initial_traces)
+                                                      get_initial_traces, 
+                                                      rotate)
+from openquake.sub.cross_sections import CrossSection, Slab2pt0
+
 
 HERE = pathlib.Path(__file__).parent.resolve()
 
@@ -59,32 +62,71 @@ class GetInitialProfiles(unittest.TestCase):
     def test_bb_across_idl(self):
 
         bb = [164.45, 223.15, 49.4, 65.75]
-
         dip_dir = 330
         spacing = 50.
-        profiles, _ = get_initial_traces(bb, dip_dir, spacing)
+        
+        x = [164.45, 165.45, 167.45, 220.25, 223.15]
+        y = [49.4, 50.1, 53.3, 60.15, 65.75]
+        xx, yy = np.meshgrid(x, y)
+        
+        depths = [50, 80, 130, 200, 350]
+        tmp = zip(xx.flatten(), yy.flatten(), depths)
+        
+        depths = [[x, y, z] for x, y, z in tmp]
+        depths = np.array(depths)
+        
+        #box = [164.45, 223.15]
+        #boy = [49.4, 65.75]
+        
+        cx = np.mean([bb[0:2]])
+        cy = np.mean([bb[2:4]])
 
+        # Rotate the grid with the fault information and get the bounding box
+        rx, ry = rotate(xx.flatten(), yy.flatten(), cx, cy, -dip_dir)
+        #bb = tmp_bb
+        r_bb, _ = get_bounding_box(rx, ry, delta=1.)
+
+        # Compute the rotated and buffered bounding box
+        dlt = 3.0
+        coox = [r_bb[0]-dlt, r_bb[1]+dlt, r_bb[1]+dlt, r_bb[0]-dlt]
+        cooy = [r_bb[2]-dlt, r_bb[2]-dlt, r_bb[3]+dlt, r_bb[3]+dlt]
+        nbbx, nbby = rotate(coox, cooy, cx, cy, dip_dir)
+        
+        
+        traces, plen = get_initial_traces(nbbx, nbby, dip_dir, spacing)
+
+        css = []
+        for pro in traces:
+            xlo = pro[0, 0]
+            xla = pro[0, 1]
+            xlo = xlo if xlo < 180 else xlo - 360
+            cs = CrossSection(xlo, xla, plen, dip_dir)
+            css.append(cs)
+        
+        slb = Slab2pt0(depths, css)
+        slb.compute_profiles(spacing/2)
+        
         if True:
             import pygmt
             import netCDF4
 
-            grid = False
+            grid = True
 
             # Reading file with strike values
-            if grid:
-                fname = HERE / 'data' / 'slab2pt0' / 'alu_slab2_dep_02.23.18.grd'
-                dat = netCDF4.Dataset(fname)
-                xx = np.array(dat.variables['x'])
-                yy = np.array(dat.variables['y'])
-                z = np.array(dat.variables['z'])
-                x, y = np.meshgrid(xx, yy)
-                mask = np.where(np.isfinite(z))
-                z = z[mask]
-                x = x[mask]
-                y = y[mask]
-                tmp = zip(x.flatten(), y.flatten(), y.flatten())
-                dd = np.array([[x, y, z] for x, y, z in tmp])
-                xx, yy = np.meshgrid(x, y)
+            #if grid:
+            #    fname = HERE / 'data' / 'slab2pt0' / 'alu_slab2_dep_02.23.18.grd'
+            #    dat = netCDF4.Dataset(fname)
+            #    xx = np.array(dat.variables['x'])
+            #    yy = np.array(dat.variables['y'])
+            #    z = np.array(dat.variables['z'])
+            #    x, y = np.meshgrid(xx, yy)
+            #    mask = np.where(np.isfinite(z))
+            #    z = z[mask]
+            #    x = x[mask]
+            #    y = y[mask]
+            #    tmp = zip(x.flatten(), y.flatten(), z.flatten())
+            #    dd = np.array([[x, y, z] for x, y, z in tmp])
+            #    xx, yy = np.meshgrid(x, y)
 
             fig = pygmt.Figure()
             buf = 5
@@ -95,13 +137,23 @@ class GetInitialProfiles(unittest.TestCase):
             fig.basemap(region=reg, projection=prjstr, frame=True)
 
             if grid:
-                idx = np.random.choice(np.arange(0, len(dd)), 1000)
-                fig.plot(x=dd[idx, 0], y=dd[idx, 1], style="c0.01c", pen="grey")
+                idx = np.random.choice(np.arange(0, len(depths)), 1000)
+                fig.plot(x=depths[idx, 0], y=depths[idx, 1], style="c0.01c", pen="grey")
 
-            for pro in profiles:
+            for pro in traces:
                 tmpx = pro[:, 0]
                 tmpx[tmpx < 0] = tmpx[tmpx < 0] + 360
                 fig.plot(x=tmpx, y=pro[:, 1], pen="red")
+            
+            for key in slb.profiles:
+                pro = slb.profiles[key]
+                if pro.shape[0] > 0:
+                    fig.plot(x=pro[:, 0],
+                         y=pro[:, 1],
+                         color=pro[:, 2],
+                         cmap=True,
+                         style="h0.025c",
+                         pen='black')
 
             # This is the bounding box
             fig.plot(x=[bb[0], bb[1], bb[1], bb[0], bb[0]],
