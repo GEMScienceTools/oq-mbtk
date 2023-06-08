561d1046b76fd4f0490fffcc5474ca257c199db9
unknown <christopher.brooks@globalquakemodel.org>
2023-06-08 16:34:59 +0200

    update

--- openquake/smt/parsers/esm23_flatfile_parser.py
+++ openquake/smt/parsers/esm23_flatfile_parser.py
@@ -153,12 +153,6 @@ class ESM23FlatfileParser(SMDatabaseReader):
             if pd.isnull(ESM23.vs30_m_s[idx]):
                 ESM23['vs30_m_s'].iloc[idx]=ESM23['vs30_m_s_wa'].iloc[idx]
         
-        # Drop if Vs30 still empty
-        Index_to_drop=np.array(ESM23.loc[ESM23['vs30_m_s'].isnull()][
-            'vs30_m_s'].index)
-        ESM23 = ESM23.drop(Index_to_drop)
-        ESM23 = ESM23.reset_index().drop(columns='index')
-        
         # Create default values for headers not considered in ESM23 format
         default_string = pd.Series(np.full(np.size(ESM23.esm_event_id),
                                            str("")))
