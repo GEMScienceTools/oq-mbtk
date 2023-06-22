9b177329c0ab4621acb77cd8aad629742923a70c
Kirsty Bayliss <kirsty.bayliss@globalquakemodel.org>
2023-06-22 13:49:50 +0200

    fixed adaptive smoothing test

--- openquake/wkf/tests/adaptive_smoothing_wkf_test.py
+++ openquake/wkf/tests/adaptive_smoothing_wkf_test.py
@@ -36,10 +36,11 @@ class AdaptiveSmoothingTestwkf(unittest.TestCase):
         cmd = f"oqm wkf wkf_adaptive_smoothing {self.fname} {fname_h3} {config} {fname_out}"
         p = subprocess.run(cmd, shell=True)
         
-        expected = np.array([1.85124547e-03, 2.32545463e-03, 9.81211408e-04, 2.59893514e-03,
-       8.80487564e-04, 2.24894579e-03, 9.83247419e-04, 1.36002573e-03,
-       1.76509281e-03, 9.73681917e-05, 4.30942107e-05, 1.29838234e-03,
-       2.90803858e-03, 3.91305325e-06, 9.19557721e-04, 2.67332510e-03])
+        expected = np.array([1.76509281e-03, 2.24894579e-03, 1.37445308e-03, 9.81211408e-04,
+       2.59893514e-03, 1.85124547e-03, 2.32545463e-03, 9.83247419e-04,
+       8.80487564e-04, 4.30942107e-05, 9.73681917e-05, 3.91305325e-06,
+       2.90803858e-03, 9.19557721e-04, 2.67332510e-03, 1.29838234e-03])
+
         # set up an expected
         computed = pd.read_csv(fname_out)
         np.testing.assert_almost_equal(expected, computed['nocc'], decimal=4)
