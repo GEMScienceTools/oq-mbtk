98f90f0e183bfae4dc8e6de20bb15b95b46a97cd
unknown <christopher.brooks@globalquakemodel.org>
2023-09-11 13:53:01 +0200

    modified smt tests to not generate plots (also set show to false as plots are usually viewed in output directory anyway as too many are generate in a residual or comparison run to be viewed in notebook directly

--- openquake/smt/comparison/utils_compare_gmpes.py
+++ openquake/smt/comparison/utils_compare_gmpes.py
@@ -701,7 +701,7 @@ def update_trellis_plots(m, i, n, l, r_vals, imt_list, dist_type):
         pyplot.ylabel(str(i) + ' (g)', fontsize='16')
     pyplot.loglog()
     pyplot.ylim(0.001, 10) # Mod if required
-    pyplot.xlim(r_vals[0], r_vals[len(r_vals)-1]) # Mod if required
+    #pyplot.xlim(r_vals[0], r_vals[len(r_vals)-1]) # Mod if required
                 
 
 ### Spectra utils
