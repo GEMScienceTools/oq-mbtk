8fb653be2831418bd21a3b7361fe209555668110
Kirsty Bayliss <kirsty.bayliss@globalquakemodel.org>
2023-11-07 10:05:59 +0100

    fixed wkf test to use temp files

--- openquake/wkf/tests/adaptive_smoothing_wkf_test.py
+++ openquake/wkf/tests/adaptive_smoothing_wkf_test.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 import openquake.mbt.tools.adaptive_smoothing as ak
 from openquake.hmtk.parsers.catalogue import CsvCatalogueParser
-
+from pathlib import Path
 
 HERE = os.path.dirname(__file__)
 #CWD = os.getcwd()
@@ -30,15 +30,21 @@ class AdaptiveSmoothingTestwkf(unittest.TestCase):
     def test_adap_smoothing_wkf(self):
         """ Test adaptive smoothing build """
 
-        tmp = tempfile.NamedTemporaryFile()
+        
+        tmpdir = Path(tempfile.gettempdir())
+        if not os.path.exists(tmpdir):
+            os.makedirs(tmpdir)
+        
+        folder_out = tempfile.mkdtemp(suffix='adapsmooth', prefix=None, dir=tmpdir)
+        fname_out = '{}/smooth_adap.csv'.format(folder_out)
+
         config = os.path.join(DATA_PATH, 'smooth_config.toml')
         fname_h3 = os.path.join(DATA_PATH, 'mapping_h2.csv')
-        fname_out = os.path.join(DATA_PATH, 'smooth_adap.csv')
-
+        
         # Run the code
         cmd = f"oqm wkf wkf_adaptive_smoothing {self.fname} {fname_h3} {config} {fname_out}"
         p = subprocess.run(cmd, shell=True)
-        
+
         expected = np.array([0.000984, 0.000983, 0.001360, 0.001375,0.002249,
         0.002325, 0.000881, 0.001852, 0.001765, 0.002598, 0.002911, 0.000004,
         0.000044, 0.000926, 0.000099, 0.002677, 0.001304])
@@ -46,4 +52,3 @@ class AdaptiveSmoothingTestwkf(unittest.TestCase):
         # set up an expected
         computed = pd.read_csv(fname_out)
         np.testing.assert_almost_equal(expected, computed['nocc'], decimal=4)
-
