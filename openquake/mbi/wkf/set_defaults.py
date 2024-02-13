f301f93b79822fa2dff13763809341ed31e8270d
Kirsty Bayliss <kirsty.bayliss@globalquakemodel.org>
2024-02-13 15:54:45 +0100

    update set_defaults to accept a list of polygons to use

--- openquake/mbi/wkf/set_defaults.py
+++ openquake/mbi/wkf/set_defaults.py
@@ -7,8 +7,13 @@ from openquake.baselib import sap
 from openquake.wkf.utils import get_list
 
 
-def main(fname_conf: str, fname_defaults: str):
+def main(fname_conf: str, fname_defaults: str,  use: str = [], skip: str = []):
 
+    if len(use) > 0:
+        use = get_list(use)
+    if len(skip) > 0:
+        skip = get_list(skip)
+        
     # Parsing config
     model = toml.load(fname_conf)
     output = copy.copy(model)
@@ -17,10 +22,12 @@ def main(fname_conf: str, fname_defaults: str):
     defaults = toml.load(fname_defaults)
 
     # Adding fields
-    if 'sources' not in output:
-        output['sources'] = {}
+    #if 'sources' not in output:
+    #    output['sources'] = {}
 
     for src_id in defaults['sources']:
+        if (len(use) and src_id not in use) or (src_id in skip):
+            continue
         if src_id not in output['sources']:
             output['sources'][src_id] = {}
         for key in defaults['sources'][src_id]:
