5dc0ca695c43dcfead22b2c906c17e92dd5ad62e
Kirsty Bayliss <kirsty.bayliss@globalquakemodel.org>
2024-02-20 13:55:05 +0100

    minor changes to norms tests

--- openquake/cat/tests/completeness/norms_test.py
+++ openquake/cat/tests/completeness/norms_test.py
@@ -30,9 +30,11 @@ import numpy as np
 from openquake.hmtk.seismicity.catalogue import Catalogue
 from openquake.hmtk.seismicity.occurrence.utils import get_completeness_counts
 from openquake.mbt.tools.model_building.dclustering import _add_defaults
-from openquake.cat.completeness.norms import get_norm_optimize_b, get_norm_optimize_c, get_norm_optimize_poisson
+from openquake.cat.completeness.norms import (
+    get_norm_optimize_b, get_norm_optimize_c, get_norm_optimize_poisson)
 
 DATA = os.path.join(os.path.dirname(__file__), 'data')
+
 
 class NormBTest(unittest.TestCase):
 
@@ -58,19 +60,11 @@ class NormBTest(unittest.TestCase):
         bval = 1.0
         cmag, t_per, n_obs = get_completeness_counts(self.cat, self.compl,
                                                      mbinw)
-        norm = get_norm_optimize_b(aval, bval, self.compl, self.cat, mbinw,
-                                   ybinw)
+        _ = get_norm_optimize_b(aval, bval, self.compl, self.cat, mbinw, ybinw)
 
     def test_case02(self):
 
         mbinw = 0.1
-        ybinw = 10.0
-        aval = 4.6
-        bval = 1.0
-
-        aval = 3.8897
-        bval = 0.8332
-
         tmp = np.loadtxt(os.path.join(DATA, 'cat_norm_02.csv'), skiprows=1,
                          delimiter=',')
 
@@ -79,30 +73,20 @@ class NormBTest(unittest.TestCase):
         cat = _add_defaults(cat)
         cat.data["dtime"] = cat.get_decimal_time()
 
-        compl = np.array([[2000, 4.4], [1990, 5.0], [1980, 5.8]])
-        #compl = np.array([[1995, 4.4], [1985, 5.4]])
-        #compl = np.array([[2000, 4.4]])
-
-        compl = np.array([[2000, 4.4], [1990, 5.5]])
-
-        compl = np.array([[2000, 4.4], [1985, 5.8]]);
-        aval = 3.8004918570326267; bval = 0.8114202323942403; rmag = 4.55; rmagu = 10.15
+        compl = np.array([[2000, 4.4], [1985, 5.8]])
+        aval = 3.8004918570326267
+        bval = 0.8114202323942403
 
         cmag, t_per, n_obs = get_completeness_counts(cat, compl, mbinw)
-        #norm = get_norm_optimize_b(aval, bval, compl, cat, mbinw, ybinw=ybinw,
-        #                           mmin=rmag, mmax=rmagu)
-        norm = get_norm_optimize_c(cat, aval, bval, compl, 2022, ref_mag = rmag)
+        norm = get_norm_optimize_c(cat, aval, bval, compl, 2022, ref_mag=4.4)
         print(f'{norm:.5e}')
+
 
     def test_poisson(self):
 
         mbinw = 0.1
-        ybinw = 10.0
         aval = 4.6
         bval = 1.0
-
-        aval = 3.8897
-        bval = 0.8332
 
         tmp = np.loadtxt(os.path.join(DATA, 'cat_norm_02.csv'), skiprows=1,
                          delimiter=',')
@@ -113,16 +97,7 @@ class NormBTest(unittest.TestCase):
         cat.data["dtime"] = cat.get_decimal_time()
 
         compl = np.array([[2000, 4.4], [1990, 5.0], [1980, 5.8]])
-        #compl = np.array([[1995, 4.4], [1985, 5.4]])
-        #compl = np.array([[2000, 4.4]])
-
-        compl = np.array([[2000, 4.4], [1990, 5.5]])
-
-        compl = np.array([[2000, 4.4], [1985, 5.8]]);
-        aval = 3.8004918570326267; bval = 0.8114202323942403; rmag = 4.55; rmagu = 10.15
 
         cmag, t_per, n_obs = get_completeness_counts(cat, compl, mbinw)
-        #norm = get_norm_optimize_b(aval, bval, compl, cat, mbinw, ybinw=ybinw,
-        #                           mmin=rmag, mmax=rmagu)
         norm = get_norm_optimize_poisson(cat, aval, bval, compl, 2022)
         print(f'{norm:.5e}')
