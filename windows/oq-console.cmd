a0fc5b91131e2830caafdd55cbe2c2357395cc44
antonioettorre <135960976+antonioettorre@users.noreply.github.com>
2024-03-04 16:31:24 +0100

    Update oq-console.cmd

--- windows/oq-console.cmd
+++ windows/oq-console.cmd
@@ -5,7 +5,7 @@ set mypath=%USERPROFILE%\mbtk
 set PATH=%mypath%;%mypath%\python3\Scripts;%PATH%
 set PY_PIP=%mypath%\python3\Scripts
 set PY_LIBS=%mypath%\python3\Lib;%mypath%\python3\Lib\site-package
-set PY_HOME=%mypath%
+set PY_HOME=%mypath%\python3
 set PYTHONUTF8=1
 echo OpenQuake environment loaded
 echo To run OpenQuake use 'oq' and 'oq engine'
