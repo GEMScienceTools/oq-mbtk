cf2212e318013916d4931c597bdb0ec62fbcd96f
antonioettorre <135960976+antonioettorre@users.noreply.github.com>
2024-03-04 16:24:19 +0100

    Update oq-console.cmd

--- windows/oq-console.cmd
+++ windows/oq-console.cmd
@@ -8,7 +8,6 @@ set PY_LIBS=%mypath%\Lib;%mypath%\Lib\site-package
 set PY_HOME=%mypath%
 set PYTHONUTF8=1
 echo OpenQuake environment loaded
-echo To see versions of installed software run 'pip freeze'
 echo To run OpenQuake use 'oq' and 'oq engine'
 cd %mypath%
 cmd /k
