679efb9170aea9146bff16049c27fa4777d626ab
antonioettorre <135960976+antonioettorre@users.noreply.github.com>
2024-03-04 16:29:27 +0100

    Update oq-console.cmd

--- windows/oq-console.cmd
+++ windows/oq-console.cmd
@@ -2,9 +2,9 @@
 setlocal
 
 set mypath=%USERPROFILE%\mbtk
-set PATH=%mypath%;%mypath%\Scripts;%PATH%
-set PY_PIP=%mypath%\Scripts
-set PY_LIBS=%mypath%\Lib;%mypath%\Lib\site-package
+set PATH=%mypath%;%mypath%\python3\Scripts;%PATH%
+set PY_PIP=%mypath%\python3\Scripts
+set PY_LIBS=%mypath%\python3\Lib;%mypath%\python3\Lib\site-package
 set PY_HOME=%mypath%
 set PYTHONUTF8=1
 echo OpenQuake environment loaded
