aa3d99c34233d96da265abfad0ec39fa84b9c2a2
unknown <christopher.brooks@globalquakemodel.org>
2024-03-18 19:19:08 +0100

    test

--- openquake/smt/sm_utils.py
+++ openquake/smt/sm_utils.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OpenQuake. If not, see <http://www.gnu.org/licenses/>.
 """
-Strong motion utilities.
+Strong motion utilities
 """
 # WARNING: this module is intended to collect functions used in various places
 # throughout the code. Consequently, try to limit the amount of stuff here and in
