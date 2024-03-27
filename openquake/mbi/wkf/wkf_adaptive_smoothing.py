729600c13496189f6707953b88e03a0e8d42d31c
Kirsty Bayliss <kirsty.bayliss@globalquakemodel.org>
2024-03-27 16:41:57 +0100

    fixing bad changes to smoothing

--- openquake/mbi/wkf/wkf_adaptive_smoothing.py
+++ openquake/mbi/wkf/wkf_adaptive_smoothing.py
@@ -44,24 +44,24 @@ def main(catalogue:str, h3_map: str, config:str, outputfile:str,  use: str = [])
     '''
 
     # Read h3 indices from mapping file
-    h3_idx = pd.read_csv(h3_map)
+    #h3_idx = pd.read_csv(h3_map)
+    h3_idx = pd.read_csv(h3_map, names = ("h3", "id"))
     
     if len(use) > 0:
+        # Note that this should still consider events outside this zone, 
+        # it will simply only return the adaptively smoothed values at 
+        # locations within the zone (should be consistent with adjacent zones)
         l1 = use
         use = get_list(use)
         use = map(int, use)
         h3_idx = h3_idx[h3_idx['id'].isin(use)]
         print("Using zones ", l1)
     
-    if 'lon' in h3_idx: 
-        locations = h3_idx[['lat','lon']]
- 
-    else:
-        # Get lat/lon locations for each h3 cell, convert to seperate lat and
-        # lon columns of dataframe
-        h3_idx['latlon'] = h3_idx.loc[:,"h3idx"].apply(h3.h3_to_geo)
-        locations = pd.DataFrame(h3_idx['latlon'].tolist())
-        locations.columns = ["lat", "lon"]
+    # Get lat/lon locations for each h3 cell, convert to seperate lat and
+    # lon columns of dataframe
+    h3_idx['latlon'] = h3_idx.loc[:,"h3"].apply(h3.h3_to_geo)
+    locations = pd.DataFrame(h3_idx['latlon'].tolist())
+    locations.columns = ["lat", "lon"]
 
     # Load config file to get smoothing parameters
     config = toml.load(config)
