b8f3ad7dec8f5575cbb5ffedfde1255c23c03d8a
Marco Pagani <marco.pagani@globalquakemodel.org>
2024-04-10 11:24:34 +0200

    adaptive_small_change

--- openquake/mbi/wkf/wkf_adaptive_smoothing.py
+++ openquake/mbi/wkf/wkf_adaptive_smoothing.py
@@ -11,55 +11,28 @@ from openquake.hmtk.parsers.catalogue import CsvCatalogueParser
 from openquake.wkf.utils import get_list
 
 
-def main(catalogue:str, h3_map: str, config:str, outputfile:str,  use: str = []):
-    '''
-    Runs an analysis of adaptive smoothed seismicity of Helmstetter et
-    al. (2007).
-
-    :param catalogue:
-        An earthquake catalogue csv file containing the following columns -
-        'longitude' - numpy.ndarray vector of longitudes
-        'latitude' - numpy.ndarray vector of latitudes
-
-    :param dict config:
-        Location of toml file with model configuration. The following
-        settings are necessary:
-        * 'kernel' - Kernel choice for adaptive smoothing. Options are
-            "Gaussian" or "PowerLaw" (string)
-        * 'n_v' - number of nearest neighbour to use for smoothing
-            distance (int). Use the Information Gain to calibrate this.
-        * 'd_i_min' - minimum smoothing distance d_i, should be chosen
-            based on location uncertainty. Default of 0.5 in Helmstetter
-            et al. (float)
-        * 'h3res' - h3 resolution for the model
-        * 'maxdist' - maximum distance to consider a neighbour
-        
-
-    :param output_file:
-        String specifying location in which to save output.
-
-    :param use:
-        Option to specify zone IDs to use in model
-
-    :returns:
-        Full smoothed seismicity data as np.ndarray, of the form
-        [Longitude, Latitude, Smoothed], a plot if required.
-    '''
+def _run_adaptive_smoothing(
+    catalogue,
+    h3_locations,
+    config,
+    outputfile,
+    use
+):
 
     # Read h3 indices from mapping file
     #h3_idx = pd.read_csv(h3_map)
     h3_idx = pd.read_csv(h3_map, names = ("h3", "id"))
-    
+
     if len(use) > 0:
-        # Note that this should still consider events outside this zone, 
-        # it will simply only return the adaptively smoothed values at 
+        # Note that this should still consider events outside this zone,
+        # it will simply only return the adaptively smoothed values at
         # locations within the zone (should be consistent with adjacent zones)
         l1 = use
         use = get_list(use)
         use = map(int, use)
         h3_idx = h3_idx[h3_idx['id'].isin(use)]
         print("Using zones ", l1)
-    
+
     # Get lat/lon locations for each h3 cell, convert to seperate lat and
     # lon columns of dataframe
     h3_idx['latlon'] = h3_idx.loc[:,"h3"].apply(h3.h3_to_geo)
@@ -83,7 +56,7 @@ def main(catalogue:str, h3_map: str, config:str, outputfile:str,  use: str = [])
     smooth = ak.AdaptiveSmoothing([locations.lon, locations.lat],
                                   grid=False, use_3d=False, use_maxdist = True)
     conf = {"kernel": config['kernel'], "n_v": config['n_v'],
-            "d_i_min": config['d_i_min'], "h3res": config['h3res'], 
+            "d_i_min": config['d_i_min'], "h3res": config['h3res'],
             "maxdist": config['maxdist']}
     out = smooth.run_adaptive_smooth(cat, conf)
     # Make output into dataframe with named columns and write to a csv
@@ -93,6 +66,49 @@ def main(catalogue:str, h3_map: str, config:str, outputfile:str,  use: str = [])
 
     out["nocc"] = out["nocc"]
     out.to_csv(outputfile, header=True)
+
+
+def main(catalogue:str, h3_map: str, config:str, outputfile:str, use: str = []):
+    '''
+    Runs an analysis of adaptive smoothed seismicity of Helmstetter et
+    al. (2007).
+
+    :param catalogue:
+        An earthquake catalogue csv file containing the following columns -
+        'longitude' - numpy.ndarray vector of longitudes
+        'latitude' - numpy.ndarray vector of latitudes
+
+    :param dict config:
+        Location of toml file with model configuration. The following
+        settings are necessary:
+        * 'kernel' - Kernel choice for adaptive smoothing. Options are
+            "Gaussian" or "PowerLaw" (string)
+        * 'n_v' - number of nearest neighbour to use for smoothing
+            distance (int). Use the Information Gain to calibrate this.
+        * 'd_i_min' - minimum smoothing distance d_i, should be chosen
+            based on location uncertainty. Default of 0.5 in Helmstetter
+            et al. (float)
+        * 'h3res' - h3 resolution for the model
+        * 'maxdist' - maximum distance to consider a neighbour
+
+
+    :param output_file:
+        String specifying location in which to save output.
+
+    :param use:
+        Option to specify zone IDs to use in model
+
+    :returns:
+        Full smoothed seismicity data as np.ndarray, of the form
+        [Longitude, Latitude, Smoothed], a plot if required.
+    '''
+
+    _run_adaptive_smoothing(
+        catalogue,
+        h3_map,
+        config,
+        outputfile,
+        use)
 
 
 descr = 'Instance of the openquake.hmtk.seismicity.catalogue.Catalogue class'
