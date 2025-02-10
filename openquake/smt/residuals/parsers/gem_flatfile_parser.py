19830b7e182444d0d1444357117738ad02deb7f3
unknown <christopher.brooks@globalquakemodel.org>
2025-02-10 14:44:27 +0100

    fix dt issues

--- openquake/smt/residuals/parsers/gem_flatfile_parser.py
+++ openquake/smt/residuals/parsers/gem_flatfile_parser.py
@@ -160,8 +160,7 @@ class GEMFlatfileParser(SMDatabaseReader):
         eq_name = metadata["event_id"]
         # Country
         # Date and time
-        eq_datetime = valid.date_time(metadata["event_time"],
-                                     "%Y-%m-%d %H:%M:%S")
+        eq_datetime = pd.to_datetime(metadata["event_time"])
         # Latitude, longitude and depth
         eq_lat = valid.latitude(metadata["ev_latitude"])
         eq_lon = valid.longitude(metadata["ev_longitude"])
