c255dbd8ba96c547d996f689511702df9cc2a9fa
unknown <christopher.brooks@globalquakemodel.org>
2025-05-09 17:56:54 +0200

    upd

--- openquake/man/tests/tools/plot_disagg_test.py
+++ openquake/man/tests/tools/plot_disagg_test.py
@@ -8,6 +8,7 @@ from openquake.man.tools.plot_3d_disagg import disagg_MRE
 
 base = os.path.abspath("")
 
+in_mre = os.path.join(base, "data", "calc_763.hdf5")
 out_mre = os.path.join(base, "..", "..", "tools", "disagg_Mag_Dist_Eps_calc_763")
 
 
@@ -15,13 +16,14 @@ class TestPlotDisaggMRE(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        cls.in_mre = in_mre
         cls.out_mre = out_mre
 
     def test_plot_disagg_MRE(self):
         """
         Check execution of 3D mag-dist-eps plotting function
         """
-        disagg_MRE(763, "Mag_Dist_Eps", None, 45)
+        disagg_MRE(self.in_mre, "Mag_Dist_Eps", None, 45)
 
     @classmethod
     def tearDownClass(cls):
