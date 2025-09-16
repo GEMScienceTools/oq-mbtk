030216a9e6573ad5c0daa3297e27723710faa4b5
unknown <christopher.brooks@globalquakemodel.org>
2025-09-16 16:08:35 +0200

    fix import

--- openquake/mbt/notebooks/sources_shallow_fault/shallow_faults.py
+++ openquake/mbt/notebooks/sources_shallow_fault/shallow_faults.py
@@ -6,7 +6,7 @@ from osgeo import ogr
 from openquake.hazardlib.source import SimpleFaultSource
 from openquake.hazardlib.geo.line import Line
 
-from openquake.man.model import read
+openquake.man.checking_utils.source_model_utils import read
 from openquake.mbt.oqt_project import OQtProject, OQtSource
 from openquake.mbt.tools.utils import _get_point_list
 
