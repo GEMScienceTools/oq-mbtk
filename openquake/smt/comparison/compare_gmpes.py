27beb6907ca7a9387bf24a255d6dcd06c1f23697
unknown <christopher.brooks@globalquakemodel.org>
2025-09-24 17:23:26 +0200

    upd

--- openquake/smt/comparison/compare_gmpes.py
+++ openquake/smt/comparison/compare_gmpes.py
@@ -94,8 +94,8 @@ class Configurations(object):
             if self.ztor != -999 and len(self.mags_eucl) != len(self.ztor):
                 raise ValueError(
                     f"If specifying ZTOR values (i.e. ZTOR != -999), the number of " \
-                    f"ZTOR values must equal the number of magnitude values specified "
-                    f"in the euclidean_analysis key ({len(self.mags_eucl)} mag values).")
+                    f"ZTOR values ({len(self.ztor)}) must equal the number of mag "
+                    f"values in the euclidean_analysis key ({len(self.mags_eucl)}).")
             
         # Get imts
         self.imt_list = [from_string(imt) for imt in config_file['general']['imt_list']]
