10f68de77dc38fbb5d875e7bc9c5e30ebde2a0e3
unknown <christopher.brooks@globalquakemodel.org>
2025-10-09 18:44:36 +0200

    restore _load_catalogue and oqtproject

--- openquake/man/checking_utils/mfds_and_rates_utils.py
+++ openquake/man/checking_utils/mfds_and_rates_utils.py
@@ -20,9 +20,11 @@ import numpy as np
 import copy
 import matplotlib.pyplot as plt
 
+from openquake.mbt.oqt_project import OQtProject
 from openquake.hazardlib.mfd.truncated_gr import TruncatedGRMFD
 from openquake.hazardlib.source.non_parametric import NonParametricSeismicSource
 from openquake.hazardlib.mfd import EvenlyDiscretizedMFD
+from openquake.mbt.tools.mfd import get_evenlyDiscretizedMFD_from_truncatedGRMFD
 
 from openquake.man.checking_utils.source_model_utils import read
 from openquake.mbt.tools.mfd import mag_to_mo
@@ -148,12 +150,35 @@ def get_mags_rates(source_model_fname: str, time_span: float):
     return mag, rate/time_span
 
 
-def mfd_from_xml(source_model_fname):
+def xml_vs_mfd(source_id, source_model_fname, model_id,
+               oqmbt_project_fname):
     """
+    :param str source_id:
+        The ID of the source to be analysed
     :param str source_model_fname:
-        The name of the xml
+        The name of the xml shapefile
+    :param str model_id:
+        The model ID
     """
-    # Read the source_model
+    #
+    # read the source_model
     src_model, info = read(source_model_fname)
-    
-    return get_total_mfd(src_model) # Total mfd sources
+    #
+    # compute total mfd sources
+    tmfd = get_total_mfd(src_model)
+    #
+    # read project
+    oqtkp = OQtProject.load_from_file(oqmbt_project_fname)
+    model_id = oqtkp.active_model_id
+    model = oqtkp.models[model_id]
+    #
+    # get source mfd
+    src = model.sources[source_id]
+    mfd = src.mfd
+    if isinstance(src.mfd, TruncatedGRMFD):
+        mfd = get_evenlyDiscretizedMFD_from_truncatedGRMFD(mfd)
+    #
+    # compute total mfd sources
+    plt.figure(figsize=(10, 8))
+    plot_mfd_cumulative(tmfd)
+    plot_mfd_cumulative(mfd, title=source_model_fname)
