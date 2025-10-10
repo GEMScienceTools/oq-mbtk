1e9c98f6feb03e032eee867103d7522653ed4668
Kirsty Bayliss <kirsty.bayliss@globalquakemodel.org>
2025-10-10 10:06:08 +0200

    restoring files (from master)

--- openquake/mbt/tools/tr/tectonic_regionalisation.py
+++ openquake/mbt/tools/tr/tectonic_regionalisation.py
@@ -3,7 +3,6 @@
 """
 import re
 import numpy as np
-from decimal import Decimal, getcontext
 
 from rtree import index
 
@@ -49,7 +48,7 @@ def set_crustal(cat, crust, sidx, delta=0, lower_depth=400):
         iii = list(sidx.nearest((lon, lat, lon, lat), 1))
         #
         # Set the crustal earthquakes
-        if Decimal(crust[iii[0], 2]+float(delta)) > Decimal(dep) and (Decimal(dep) <= Decimal(lower_depth)):
+        if (crust[iii[0], 2]+float(delta) > dep) and (dep <= lower_depth):
             treg[idx] = True
         data.append([dep, crust[iii[0], 2]])
     return treg, data
