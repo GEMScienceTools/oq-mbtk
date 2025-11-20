82ecf2f03dae07e4c9ae087ea4d3b0a069470679
unknown <christopher.brooks@globalquakemodel.org>
2025-11-20 22:14:54 +0100

    upd

--- openquake/smt/residuals/parsers/gem_flatfile_parser.py
+++ openquake/smt/residuals/parsers/gem_flatfile_parser.py
@@ -313,9 +313,10 @@ class GEMFlatfileParser(SMDatabaseReader):
         # Basic site/station information
         network_code = metadata["network_code"].strip()
         station_code = metadata["station_code"].strip()
-        if station_code == 'ST_UPTAC':
-            breakpoint()
-        site_id = "{:s}-{:s}".format(network_code, station_code)
+        if len(network_code):
+            site_id = "{:s}".format(station_code)
+        else:
+            site_id = "{:s}-{:s}".format(network_code, station_code)
         site_lon = utils.longitude(metadata["st_longitude"])
         site_lat = utils.latitude(metadata["st_latitude"])
         elevation = utils.vfloat(metadata["st_elevation"], "st_elevation")
