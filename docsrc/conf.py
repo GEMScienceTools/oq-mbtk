a110421407db2433778fa1173e09a8d678fc5b27
Kirsty Bayliss <kirsty.bayliss@globalquakemodel.org>
2026-01-21 12:23:02 +0100

    adding imports

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -15,7 +15,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join('..', 'openquake')))
+sys.path.insert(0, os.path.abspath('..'))
+
+from openquake import cat, wkf, mbt, mbi, aft, fnm, ghm, man
 
 # -- Project information -----------------------------------------------------
 
