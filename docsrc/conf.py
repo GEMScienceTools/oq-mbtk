1c893dd7aa58fa909ebd1bd34a2af87bae017ec7
Kirsty Bayliss <kirsty.bayliss@globalquakemodel.org>
2026-04-30 16:09:59 +0200

    reverting conf.py changes

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -12,15 +12,15 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'OpenQuake Model Building Toolkit Suite'
-copyright = '2020-2026, GEM Hazard'
+copyright = '2020-2022, GEM Hazard'
 author = 'GEM Hazard'
 
 # The short X.Y version
