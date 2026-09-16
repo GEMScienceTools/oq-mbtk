e36dcfff18bf6378dcae1e7e94750ddd77d4a0f0
unknown <christopher.brooks@globalquakemodel.org>
2026-09-16 12:15:15 +0200

    use literal strings

--- openquake/wkf/compute_gr_params.py
+++ openquake/wkf/compute_gr_params.py
@@ -559,8 +559,8 @@ def _weichert_plot(cent_mag, n_obs, binw, t_per, ex_rates_scaled,
         tmp_bgr = bval_wei + eps_bgr * bval_sigma
         tmp_agr = _get_agr(tmp_bgr, tmp_rate, ref_mag, mmax=None)
         xmag, exra = fun(tmp_agr, tmp_bgr, cent_mag, binw, mmax)
-        lab = f'rate m$_{{{ref_mag:.1f}}}${eps_rate:+.1f}$\sigma$'
-        lab += f' bgr{eps_bgr:+.1f}$\sigma$'
+        lab = rf'rate m$_{{{ref_mag:.1f}}}${eps_rate:+.1f}$\sigma$'
+        lab += rf' bgr{eps_bgr:+.1f}$\sigma$'
         plt.plot(xmag, exra, ls='-.', color='orange', label=lab,
                  alpha=alpha)
 
@@ -571,8 +571,8 @@ def _weichert_plot(cent_mag, n_obs, binw, t_per, ex_rates_scaled,
         tmp_bgr = bval_wei + eps_bgr * bval_sigma
         tmp_agr = _get_agr(tmp_bgr, tmp_rate, ref_mag, mmax=None)
         xmag, exra = fun(tmp_agr, tmp_bgr, cent_mag, binw, mmax)
-        lab = f'rate m$_{{{ref_mag:.1f}}}${eps_rate:+.1f}$\sigma$'
-        lab += f' bgr{eps_bgr:+.1f}$\sigma$'
+        lab = rf'rate m$_{{{ref_mag:.1f}}}${eps_rate:+.1f}$\sigma$'
+        lab += rf' bgr{eps_bgr:+.1f}$\sigma$'
         plt.plot(xmag, exra, ls='-.', color='purple', label=lab,
                  alpha=alpha)
 
@@ -583,8 +583,8 @@ def _weichert_plot(cent_mag, n_obs, binw, t_per, ex_rates_scaled,
         tmp_bgr = bval_wei + eps_bgr * bval_sigma
         tmp_agr = _get_agr(tmp_bgr, tmp_rate, ref_mag, mmax=None)
         xmag, exra = fun(tmp_agr, tmp_bgr, cent_mag, binw, mmax)
-        lab = f'rate m$_{{{ref_mag:.1f}}}${eps_rate:+.1f}$\sigma$'
-        lab += f' bgr{eps_bgr:+.1f}$\sigma$'
+        lab = rf'rate m$_{{{ref_mag:.1f}}}${eps_rate:+.1f}$\sigma$'
+        lab += rf' bgr{eps_bgr:+.1f}$\sigma$'
         plt.plot(xmag, exra, ls=':', color='orange', label=lab,
                  alpha=alpha)
 
@@ -595,8 +595,8 @@ def _weichert_plot(cent_mag, n_obs, binw, t_per, ex_rates_scaled,
         tmp_bgr = bval_wei + eps_bgr * bval_sigma
         tmp_agr = _get_agr(tmp_bgr, tmp_rate, ref_mag, mmax=None)
         xmag, exra = fun(tmp_agr, tmp_bgr, cent_mag, binw, mmax)
-        lab = f'rate m$_{{{ref_mag:.1f}}}${eps_rate:+.1f}$\sigma$'
-        lab += f' bgr{eps_bgr:+.1f}$\sigma$'
+        lab = rf'rate m$_{{{ref_mag:.1f}}}${eps_rate:+.1f}$\sigma$'
+        lab += rf' bgr{eps_bgr:+.1f}$\sigma$'
         plt.plot(xmag, exra, ls=':', color='purple', label=lab,
                  alpha=alpha)
 
